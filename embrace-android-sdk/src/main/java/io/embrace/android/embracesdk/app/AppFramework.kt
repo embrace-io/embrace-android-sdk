b61f0d0c0dd1741c1a4f81897e882605127e2cb5
Hanson Ho <hanson.ho@embrace.io>
2023-10-18 23:06:48 -0700

    Convert to Kotlin

---
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/app/AppFramework.kt
@@ -0,0 +1,11 @@
+package io.embrace.android.embracesdk.app
+
+/**
+ * The AppFramework that is in use.
+ */
+public enum class AppFramework(public val value: Int) {
+    NATIVE(1),
+    REACT_NATIVE(2),
+    UNITY(3),
+    FLUTTER(4)
+}
--- embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/app/LastRunEndState.kt
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/app/LastRunEndState.kt
@@ -1,9 +1,9 @@
-package io.embrace.android.embracesdk.app;
+package io.embrace.android.embracesdk.app
 
 /**
  * Enum representing the end state of the last run of the application.
  */
-public enum LastRunEndState {
+public enum class LastRunEndState(public val value: Int) {
     /**
      * The SDK has not been started yet.
      */
@@ -17,15 +17,5 @@ public enum LastRunEndState {
     /**
      * The last run did not result in a crash.
      */
-    CLEAN_EXIT(2);
-
-    private final int value;
-
-    LastRunEndState(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
+    CLEAN_EXIT(2)
 }

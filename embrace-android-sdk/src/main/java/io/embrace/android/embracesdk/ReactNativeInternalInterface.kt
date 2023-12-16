be9bd36163377348bd9ee332021314c881199b2e
Hanson Ho <hanson.ho@embrace.io>
2023-12-16 12:38:59 -0800

    Remove RN methods from EmbraceImpl

--- embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/ReactNativeInternalInterface.kt
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/ReactNativeInternalInterface.kt
@@ -42,4 +42,26 @@ internal interface ReactNativeInternalInterface : EmbraceInternalInterface {
      * See [Embrace.setJavaScriptBundleURL]
      */
     fun setJavaScriptBundleUrl(context: Context, url: String)
+
+    /**
+     * Logs a React Native Redux Action - this is not intended for public use.
+     */
+    fun logRnAction(
+        name: String,
+        startTime: Long,
+        endTime: Long,
+        properties: Map<String?, Any?>,
+        bytesSent: Int,
+        output: String
+    )
+
+    /**
+     * Logs the fact that a particular view was entered.
+     *
+     * If the previously logged view has the same name, a duplicate view breadcrumb will not be
+     * logged.
+     *
+     * @param screen the name of the view to log
+     */
+    fun logRnView(screen: String)
 }

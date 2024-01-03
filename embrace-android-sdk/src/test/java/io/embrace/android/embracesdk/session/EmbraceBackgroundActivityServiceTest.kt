1a546441cf9c780b4d4eed67446a480481458993
Jamie Lynch <fractalwrench@gmail.com>
2024-01-03 15:32:19 +0000

    poc: encapsulate executors

--- embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/session/EmbraceBackgroundActivityServiceTest.kt
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/session/EmbraceBackgroundActivityServiceTest.kt
@@ -104,7 +104,7 @@ internal class EmbraceBackgroundActivityServiceTest {
         service.onBackground(clock.now())
 
         val payload = checkNotNull(service.backgroundActivity)
-        assertEquals(Session.LifeEventType.BKGND_STATE, payload.startType)
+        assertEquals(BackgroundActivity.LifeEventType.BKGND_STATE, payload.startType)
         assertEquals(5, payload.number)
         assertEquals(payload.sessionId, metadataService.activeSessionId)
     }
@@ -122,7 +122,7 @@ internal class EmbraceBackgroundActivityServiceTest {
         assertEquals(2, deliveryService.saveBackgroundActivityInvokedCount)
         assertEquals(1, deliveryService.sendBackgroundActivitiesInvokedCount)
         val payload = checkNotNull(deliveryService.lastSavedBackgroundActivities.last())
-        assertEquals(5, payload.session.number)
+        assertEquals(5, payload.backgroundActivity.number)
     }
 
     @Test
@@ -358,7 +358,7 @@ internal class EmbraceBackgroundActivityServiceTest {
             ndkService,
             clock,
             collator,
-            BackgroundWorker(blockableExecutorService)
+            blockableExecutorService
         )
     }
 }

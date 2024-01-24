3c4545fe1746778402dec8cdc8fb6b309f3586eb
Hanson Ho <hanson.ho@embrace.io>
2024-01-23 16:25:29 -0800

    Clear completed span in SpansRepository instance when the service is flushed

--- embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/internal/spans/SpansRepository.kt
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/internal/spans/SpansRepository.kt
@@ -1,14 +1,15 @@
 package io.embrace.android.embracesdk.internal.spans
 
 import io.embrace.android.embracesdk.spans.EmbraceSpan
-import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicBoolean
 
 /**
  * Allows the tracking of [EmbraceSpan] instances so that their references can be retrieved with its associated spanId
  */
 internal class SpansRepository {
     private val activeSpans: MutableMap<String, EmbraceSpan> = mutableMapOf()
-    private val completedSpans: MutableMap<String, EmbraceSpan> = ConcurrentHashMap()
+    private val completedSpans: MutableMap<String, EmbraceSpan> = mutableMapOf()
+    private val clearing = AtomicBoolean(false)
 
     /**
      * Track the [EmbraceSpan] if it has been started and it's not already tracked.
@@ -21,7 +22,7 @@ internal class SpansRepository {
                     if (embraceSpan.isRecording) {
                         activeSpans[spanId] = embraceSpan
                     } else {
-                        completedSpans[spanId] = embraceSpan
+                        setCompletedSpan(spanId, embraceSpan)
                     }
                 }
             }
@@ -33,8 +34,8 @@ internal class SpansRepository {
      */
     fun trackedSpanStopped(spanId: String) {
         synchronized(activeSpans) {
-            activeSpans[spanId]?.takeIf { !it.isRecording }?.let { activeSpans.remove(spanId) }?.let { span ->
-                completedSpans[spanId] = span
+            activeSpans[spanId]?.takeIf { !it.isRecording }?.let { activeSpans.remove(spanId) }?.let { embraceSpan ->
+                setCompletedSpan(spanId, embraceSpan)
             }
         }
     }
@@ -46,7 +47,7 @@ internal class SpansRepository {
         synchronized(activeSpans) {
             activeSpans[spanId]?.let { return it }
         }
-        return completedSpans[spanId]
+        return getCompletedSpan(spanId)
     }
 
     /**
@@ -57,8 +58,33 @@ internal class SpansRepository {
     /**
      * Get a list of completed spans that are being tracked.
      */
-    fun getCompletedSpans(): List<EmbraceSpan> = completedSpans.values.toList()
+    fun getCompletedSpans(): List<EmbraceSpan> = synchronized(completedSpans) { completedSpans.values.toList() }
+
+    fun clearCompletedSpans() {
+        synchronized(completedSpans) {
+            clearing.set(true)
+            completedSpans.clear()
+            clearing.set(false)
+        }
+    }
+
+    /**
+     * Prevent locking and blocking on a get, always return null if we are clearing completed spans
+     */
+    private fun getCompletedSpan(spanId: String): EmbraceSpan? {
+        return if (!clearing.get()) {
+            completedSpans[spanId]
+        } else {
+            null
+        }
+    }
+
+    private fun setCompletedSpan(spanId: String, span: EmbraceSpan) {
+        synchronized(completedSpans) {
+            completedSpans[spanId] = span
+        }
+    }
 
     private fun notTracked(spanId: String): Boolean =
-        synchronized(activeSpans) { activeSpans[spanId] == null && completedSpans[spanId] == null }
+        synchronized(activeSpans) { activeSpans[spanId] == null && getCompletedSpan(spanId) == null }
 }

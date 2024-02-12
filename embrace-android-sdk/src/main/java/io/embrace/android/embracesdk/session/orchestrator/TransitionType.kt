a6fc64175948ff433fd1b0daa8b615b78c9a01ee
Jamie Lynch <fractalwrench@gmail.com>
2024-02-12 15:38:25 +0000

    docs: add docs on state transitions

--- embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/session/orchestrator/TransitionType.kt
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/session/orchestrator/TransitionType.kt
@@ -2,9 +2,39 @@ package io.embrace.android.embracesdk.session.orchestrator
 
 import io.embrace.android.embracesdk.session.lifecycle.ProcessState
 
+/**
+ * Defines all the possible state transitions that can happen with a session.
+ */
 internal enum class TransitionType {
-    INITIAL, END_MANUAL, ON_FOREGROUND, ON_BACKGROUND, CRASH;
 
+    /**
+     * The initial state transition. This will be from undefined to either foreground/background.
+     */
+    INITIAL,
+
+    /**
+     * Manual end request - only respected for foreground sessions.
+     */
+    END_MANUAL,
+
+    /**
+     * When lifecycle callbacks detect the process has entered the foreground.
+     */
+    ON_FOREGROUND,
+
+    /**
+     * When lifecycle callbacks detect the process has entered the background.
+     */
+    ON_BACKGROUND,
+
+    /**
+     * When a JVM unhandled exception is detected.
+     */
+    CRASH;
+
+    /**
+     * Returns the end state of the session based on the current state and the transition type.
+     */
     fun endState(currentState: ProcessState): ProcessState = when (this) {
         ON_FOREGROUND -> ProcessState.FOREGROUND
         ON_BACKGROUND -> ProcessState.BACKGROUND

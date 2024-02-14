ebc36f9d47d7ac1618d5b2efedf44c8e61c30506
Leandro Godon <leandro.godon@embrace.io>
2024-02-14 18:46:37 -0300

    Added tests

---
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/fixtures/LogTestFixtures.kt
@@ -0,0 +1,14 @@
+package io.embrace.android.embracesdk.fixtures
+
+import io.embrace.android.embracesdk.internal.logs.EmbraceBody
+import io.embrace.android.embracesdk.internal.logs.EmbraceLogRecordData
+
+internal val testLog = EmbraceLogRecordData(
+    traceId = "ceadd56622414a06ae382e4e5a70bcf7",
+    spanId = "50fbbe95362a430ba87ebd0132262ff1",
+    timeUnixNanos = 1681972471806000000L,
+    severityNumber = 9,
+    severityText = "INFO",
+    body = EmbraceBody(message = "test log message"),
+    attributes = mapOf(Pair("test1", "value1"), Pair("test2", "value2"))
+)

340adc9b27c32f79e5b37223a80642d967771e1e
Leandro Godon <leandro.godon@embrace.io>
2024-02-20 02:30:00 -0300

    Refactored tests

--- embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/internal/logs/LogOrchestratorTest.kt
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/internal/logs/LogOrchestratorTest.kt
@@ -1,13 +1,16 @@
 package io.embrace.android.embracesdk.internal.logs
 
+import io.embrace.android.embracesdk.comms.delivery.DeliveryService
 import io.embrace.android.embracesdk.concurrency.BlockingScheduledExecutorService
 import io.embrace.android.embracesdk.fakes.FakeClock
+import io.embrace.android.embracesdk.fakes.FakeLogRecordData
 import io.embrace.android.embracesdk.worker.ScheduledWorker
-import io.mockk.every
 import io.mockk.mockk
-import io.mockk.slot
 import io.mockk.verify
-import org.junit.Assert.assertNotNull
+import io.opentelemetry.sdk.logs.data.LogRecordData
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
@@ -18,89 +21,72 @@ internal class LogOrchestratorTest {
     }
 
     private lateinit var logOrchestrator: LogOrchestrator
-    private lateinit var onLogsStored: () -> Unit
 
     private lateinit var executorService: BlockingScheduledExecutorService
     private lateinit var scheduledWorker: ScheduledWorker
     private val clock = FakeClock()
-    private val logSink: LogSinkImpl = mockk(relaxed = true)
+    private val logSink: LogSinkImpl = LogSinkImpl()
+    private val deliveryService: DeliveryService = mockk(relaxed = true)
 
     @Before
     fun setUp() {
         executorService = BlockingScheduledExecutorService()
         scheduledWorker = ScheduledWorker(executorService)
         clock.setCurrentTime(now)
-        val onLogsStoredSlot = slot<() -> Unit>()
-        every {
-            logSink.callOnLogsStored(capture(onLogsStoredSlot))
-        } returns Unit
-        logOrchestrator = LogOrchestrator(scheduledWorker, clock, logSink)
-        onLogsStored = onLogsStoredSlot.captured
-    }
-
-    @Test
-    fun `a listener is set on initialization`() {
-        assertNotNull(onLogsStored)
+        logOrchestrator = LogOrchestrator(scheduledWorker, clock, logSink, deliveryService)
     }
 
     @Test
     fun `send a batch of logs when the batch max size is reached`() {
-        val logs = mutableListOf<EmbraceLogRecordData>()
-        every { logSink.completedLogs() } returns logs
+        val logs = mutableListOf<LogRecordData>()
 
         // Fill the sink with max batch size - 1 logs
         repeat(49) {
-            logs.add(mockk())
+            logs.add(FakeLogRecordData())
         }
-        onLogsStored()
+        logSink.storeLogs(logs.toList())
 
         // Verify the logs are not sent
-        verify(exactly = 0) { logSink.flushLogs() }
-        // TODO Verify on a mocked DeliveryService when available
+        assertEquals(49, logSink.completedLogs().size)
+        verify(exactly = 0) { deliveryService.sendLogs(any()) }
 
         // Add one more log to reach max batch size
-        logs.add(mockk())
-        onLogsStored()
+        logSink.storeLogs(listOf(FakeLogRecordData()))
 
         // Verify the logs are sent
-        verify { logSink.flushLogs() }
-        // TODO Verify on a mocked DeliveryService when available
+        assertTrue(logSink.completedLogs().isEmpty())
+        verify { deliveryService.sendLogs(any()) }
     }
 
     @Test
     fun `logs are sent after inactivity time has passed`() {
-        every { logSink.completedLogs() } returns listOf(mockk())
+        logSink.storeLogs(listOf(FakeLogRecordData()))
 
-        onLogsStored()
         moveTimeAhead(2500L)
 
         // Verify the logs are sent
-        verify { logSink.flushLogs() }
-        // TODO Verify on a mocked DeliveryService when availables
+        assertTrue(logSink.completedLogs().isEmpty())
+        verify { deliveryService.sendLogs(any()) }
     }
 
     @Test
     fun `logs are sent after batch time has passed`() {
-        val logs = mutableListOf<EmbraceLogRecordData>()
-        every { logSink.completedLogs() } returns logs
-
         val timeStep = 1100L
 
         repeat(4) {
-            logs.add(mockk())
-            onLogsStored()
+            logSink.storeLogs(listOf(FakeLogRecordData()))
             moveTimeAhead(timeStep)
         }
 
         // Verify no logs have been sent
-        verify(exactly = 0) { logSink.flushLogs() }
-        // TODO Verify on a mocked DeliveryService when available
+        assertFalse(logSink.completedLogs().isEmpty())
+        verify(exactly = 0) { deliveryService.sendLogs(any()) }
 
         moveTimeAhead(timeStep)
 
         // Verify the logs are sent
-        verify { logSink.flushLogs() }
-        // TODO Verify on a mocked DeliveryService when available
+        assertTrue(logSink.completedLogs().isEmpty())
+        verify { deliveryService.sendLogs(any()) }
     }
 
     private fun moveTimeAhead(timeStep: Long) {

4975674b55f367ab19c77f09e72fb1a41f758cbe
Leandro Godon <leandro.godon@embrace.io>
2024-02-20 14:15:25 -0300

    Changed the tests to use fake objects instead of mocks.

--- embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/internal/logs/EmbraceLogRecordExporterTest.kt
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/internal/logs/EmbraceLogRecordExporterTest.kt
@@ -1,22 +1,21 @@
 package io.embrace.android.embracesdk.internal.logs
 
 import io.embrace.android.embracesdk.fakes.FakeLogRecordData
-import io.mockk.mockk
-import io.mockk.verify
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Test
 
 internal class EmbraceLogRecordExporterTest {
 
     @Test
     fun `export() should store logs in LogSink`() {
-        val logSink: LogSink = mockk(relaxed = true)
+        val logSink: LogSink = LogSinkImpl()
         val embraceLogRecordExporter = EmbraceLogRecordExporter(logSink)
         val logRecordData = FakeLogRecordData()
 
         embraceLogRecordExporter.export(listOf(logRecordData))
 
-        verify {
-            logSink.storeLogs(listOf(logRecordData))
-        }
+        assertFalse(logSink.completedLogs().isEmpty())
+        assertEquals(EmbraceLogRecordData(logRecordData), logSink.completedLogs()[0])
     }
 }
--- embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/internal/logs/EmbraceLogRecordProcessorTest.kt
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/internal/logs/EmbraceLogRecordProcessorTest.kt
@@ -1,24 +1,22 @@
 package io.embrace.android.embracesdk.internal.logs
 
+import io.embrace.android.embracesdk.fakes.FakeLogRecordExporter
 import io.embrace.android.embracesdk.fakes.FakeReadWriteLogRecord
 import io.mockk.mockk
-import io.mockk.verify
 import io.opentelemetry.sdk.logs.ReadWriteLogRecord
-import io.opentelemetry.sdk.logs.export.LogRecordExporter
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 internal class EmbraceLogRecordProcessorTest {
 
     @Test
     fun `onEmit() should call export() on the LogRecordExporter`() {
-        val logRecordExporter = mockk<LogRecordExporter>(relaxed = true)
+        val logRecordExporter = FakeLogRecordExporter()
         val logRecordProcessor = EmbraceLogRecordProcessor(logRecordExporter)
         val readWriteLogRecord: ReadWriteLogRecord = FakeReadWriteLogRecord()
 
         logRecordProcessor.onEmit(mockk(), readWriteLogRecord)
 
-        verify {
-            logRecordExporter.export(mutableListOf(readWriteLogRecord.toLogRecordData()))
-        }
+        assertEquals(readWriteLogRecord.toLogRecordData(), logRecordExporter.exportedLogs?.first())
     }
 }

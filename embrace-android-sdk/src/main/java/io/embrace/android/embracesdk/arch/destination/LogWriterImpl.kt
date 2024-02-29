bd8cbddb1d006448219bfdfb3a4291009885c4ee
Leandro Godon <leandro.godon@embrace.io>
2024-02-29 17:25:35 -0300

    Changed to the new implementation of LogWriter and LogEventData.

--- embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/arch/destination/LogWriterImpl.kt
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/arch/destination/LogWriterImpl.kt
@@ -1,8 +1,23 @@
 package io.embrace.android.embracesdk.arch.destination
 
-internal class LogWriterImpl : LogWriter {
+import io.opentelemetry.api.common.AttributeKey
+import io.opentelemetry.api.logs.Logger
+import java.util.concurrent.TimeUnit
+
+internal class LogWriterImpl(private val logger: Logger) : LogWriter {
 
     override fun <T> addLog(log: T, mapper: T.() -> LogEventData) {
-        // no-op
+        val logEventData = log.mapper()
+        val builder = logger.logRecordBuilder()
+            .setBody(logEventData.message)
+            .setSeverity(logEventData.severity)
+            .setSeverityText(logEventData.severityText)
+            .setTimestamp(logEventData.startTimeMs, TimeUnit.MILLISECONDS)
+
+        logEventData.attributes?.forEach {
+            builder.setAttribute(AttributeKey.stringKey(it.key), it.value)
+        }
+
+        builder.emit()
     }
 }

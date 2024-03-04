4ff32a86f992d7553294bf1de208963b03afa7e2
Jamie Lynch <fractalwrench@gmail.com>
2024-03-04 11:18:02 +0000

    feat: add custom breadcrumb data source

---
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/capture/crumbs/CustomBreadcrumbDataSourceTest.kt
@@ -0,0 +1,53 @@
+package io.embrace.android.embracesdk.capture.crumbs
+
+import io.embrace.android.embracesdk.fakes.FakeConfigService
+import io.embrace.android.embracesdk.fakes.FakeCurrentSessionSpan
+import io.embrace.android.embracesdk.internal.clock.millisToNanos
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+internal class CustomBreadcrumbDataSourceTest {
+
+    private lateinit var source: CustomBreadcrumbDataSource
+    private lateinit var writer: FakeCurrentSessionSpan
+
+    @Before
+    fun setUp() {
+        writer = FakeCurrentSessionSpan()
+        source = CustomBreadcrumbDataSource(
+            FakeConfigService(),
+            writer
+        )
+    }
+
+    @Test
+    fun `add invalid breadcrumb`() {
+        source.logCustom("", 0)
+        assertEquals(0, writer.addedEvents.size)
+    }
+
+    @Test
+    fun `add breadcrumb`() {
+        source.logCustom("Hello, world!", 15000000000)
+        with(writer.addedEvents.single()) {
+            assertEquals("custom-breadcrumb", spanName)
+            assertEquals(15000000000.millisToNanos(), spanStartTimeMs)
+            assertEquals(
+                mapOf(
+                    "emb.type" to "system.breadcrumb",
+                    "message" to "Hello, world!"
+                ),
+                attributes
+            )
+        }
+    }
+
+    @Test
+    fun `limit not exceeded`() {
+        repeat(150) { k ->
+            source.logCustom("Crumb #$k", 15000000000)
+        }
+        assertEquals(100, writer.addedEvents.size)
+    }
+}

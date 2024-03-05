b61f5a6bfb5b7bc89aea1d382d5c84672273d2f6
Jamie Lynch <fractalwrench@gmail.com>
2024-03-05 15:10:14 +0000

    feat: capture metadata in new envelope

---
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/capture/envelope/metadata/EnvelopeMetadataSourceImpl.kt
@@ -0,0 +1,24 @@
+package io.embrace.android.embracesdk.capture.envelope.metadata
+
+import io.embrace.android.embracesdk.capture.user.UserService
+import io.embrace.android.embracesdk.internal.payload.EnvelopeMetadata
+import java.util.Locale
+import java.util.TimeZone
+
+internal class EnvelopeMetadataSourceImpl(
+    private val userService: UserService,
+) : EnvelopeMetadataSource {
+
+    override fun getEnvelopeMetadata(): EnvelopeMetadata {
+        val userInfo = userService.getUserInfo()
+
+        return EnvelopeMetadata(
+            userId = userInfo.userId,
+            email = userInfo.email,
+            username = userInfo.username,
+            personas = userInfo.personas ?: emptySet(),
+            timezoneDescription = TimeZone.getDefault().id,
+            locale = Locale.getDefault().language + "_" + Locale.getDefault().country
+        )
+    }
+}

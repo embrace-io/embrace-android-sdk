12ad8b6e859118567cd5170bd299de2cc8ccaf86
nelsitoPuglisi <nelson.puglisi@protonmail.com>
2024-03-06 16:13:37 -0300

    Populates EnvelopeResource

--- embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/capture/envelope/resource/EnvelopeResourceSourceImplTest.kt
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/capture/envelope/resource/EnvelopeResourceSourceImplTest.kt
@@ -1,11 +1,46 @@
 package io.embrace.android.embracesdk.capture.envelope.resource
 
+import io.embrace.android.embracesdk.fakes.FakeMetadataService
+import io.embrace.android.embracesdk.internal.payload.EnvelopeResource
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 internal class EnvelopeResourceSourceImplTest {
 
-    @Test(expected = NotImplementedError::class)
-    fun `test source`() {
-        EnvelopeResourceSourceImpl().getEnvelopeResource()
+    @Test
+    fun getEnvelopeResource() {
+        val metadataService = FakeMetadataService()
+        val source = EnvelopeResourceSourceImpl(
+            metadataService.getDeviceInfo(),
+            metadataService.getAppInfo(),
+            metadataService
+        )
+        val envelope = source.getEnvelopeResource()
+
+        assertEquals("1.0.0", envelope.appVersion)
+        assertEquals(EnvelopeResource.AppFramework.NATIVE, envelope.appFramework)
+        assertEquals("com.embrace.fake", envelope.appEcosystemId)
+        assertEquals("100", envelope.buildId)
+        assertEquals("release", envelope.buildType)
+        assertEquals("oem", envelope.buildFlavor)
+        assertEquals("prod", envelope.environment)
+        assertEquals("5ac7fe", envelope.bundleVersion)
+        assertEquals("5.11.0", envelope.sdkVersion)
+        assertEquals(53, envelope.sdkSimpleVersion)
+        assertEquals("fakeReactNativeBundleId", envelope.reactNativeBundleId)
+        assertEquals("js", envelope.javascriptPatchNumber)
+        assertEquals("1.2.0", envelope.hostedSdkVersion)
+        assertEquals("19", envelope.hostedPlatformVersion)
+        assertEquals("5092abc", envelope.unityBuildId)
+        assertEquals("Samsung", envelope.deviceManufacturer)
+        assertEquals("SM-G950U", envelope.deviceModel)
+        assertEquals("arm64-v8a", envelope.deviceArchitecture)
+        assertEquals(false, envelope.jailbroken)
+        assertEquals(10000000L, envelope.diskTotalCapacity)
+        assertEquals("Android", envelope.osType)
+        assertEquals("8.0.0", envelope.osVersion)
+        assertEquals("26", envelope.osCode)
+        assertEquals("1080x720", envelope.screenResolution)
+        assertEquals(8, envelope.numCores)
     }
 }

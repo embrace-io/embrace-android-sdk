4c3ec090fe784e764db019ba5805e01324e7e0bc
nelsitoPuglisi <nelson.puglisi@protonmail.com>
2024-03-06 16:02:35 -0300

    remove useless tests

--- embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/EmbraceMemoryServiceTest.kt
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/EmbraceMemoryServiceTest.kt
@@ -1,29 +1,20 @@
 package io.embrace.android.embracesdk
 
-import android.app.ActivityManager
 import io.embrace.android.embracesdk.capture.memory.EmbraceMemoryService
 import io.embrace.android.embracesdk.fakes.FakeClock
-import io.embrace.android.embracesdk.fakes.FakeMemoryCleanerService
-import io.embrace.android.embracesdk.fakes.system.mockActivityManager
-import io.embrace.android.embracesdk.session.MemoryCleanerService
 import io.mockk.unmockkAll
 import org.junit.After
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertThrows
 import org.junit.Before
 import org.junit.Test
 
 internal class EmbraceMemoryServiceTest {
 
     private lateinit var embraceMemoryService: EmbraceMemoryService
-    private var activityManager: ActivityManager? = null
-    private var memoryCleanerService: MemoryCleanerService? = null
     private val fakeClock = FakeClock()
 
     @Before
     fun setUp() {
-        activityManager = mockActivityManager()
-        memoryCleanerService = FakeMemoryCleanerService()
         fakeClock.setCurrentTime(100L)
         embraceMemoryService = EmbraceMemoryService(fakeClock)
     }
@@ -31,27 +22,6 @@ internal class EmbraceMemoryServiceTest {
     @After
     fun tearDown() {
         unmockkAll()
-    }
-
-    @Test
-    fun `test memory service initialization throws an exception if activityManager is null`() {
-        memoryCleanerService = null
-        activityManager = null
-        assertThrows(
-            IllegalStateException::class.java
-        ) {
-            checkNotNull(memoryCleanerService) { "memoryCleanerService must not be null" }
-        }
-    }
-
-    @Test
-    fun `test memory service initialization throws an exception if memoryCleanerService is null`() {
-        activityManager = null
-        assertThrows(
-            IllegalStateException::class.java
-        ) {
-            checkNotNull(activityManager) { "activityManager must not be null" }
-        }
     }
 
     @Test

5da4bc692ee34b8d0eeaa87045cb0b3ee4e7b84f
embrace-ci <embrace-ci@users.noreply.github.com>
2024-03-06 13:43:27 +0000

    CI/CD: Automatically generated documentation for 6.4.0

--- docs/scripts/sourceset_dependencies.js
+++ docs/scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies='{":embrace-android-sdk:dokkaHtml/androidTestRelease":[],":embrace-android-sdk:dokkaHtml/debug":[],":embrace-android-sdk:dokkaHtml/main":[],":embrace-android-sdk:dokkaHtml/release":[],":embrace-android-sdk:dokkaHtml/testFixtures":[],":embrace-android-sdk:dokkaHtml/testFixturesDebug":[],":embrace-android-sdk:dokkaHtml/testFixturesRelease":[]}'
+sourceset_dependencies='{":embrace-android-sdk:dokkaHtml/debug":[],":embrace-android-sdk:dokkaHtml/main":[],":embrace-android-sdk:dokkaHtml/release":[]}'

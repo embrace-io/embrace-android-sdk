a37eedf7e5d7a40597210f0d5d7a5ae3f6b9e434
nelsitoPuglisi <nelson.puglisi@protonmail.com>
2024-03-08 14:20:16 -0300

    move from MetadataUtils to Device

---
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/fakes/FakeDevice.kt
@@ -0,0 +1,15 @@
+package io.embrace.android.embracesdk.fakes
+
+import io.embrace.android.embracesdk.capture.envelope.resource.Device
+
+public class FakeDevice(
+    override var isJailbroken: Boolean? = false,
+    override var screenResolution: String = "1920x1080",
+    override val manufacturer: String? = "Samsung",
+    override val model: String? = "Galaxy S10",
+    override val operatingSystemType: String = "Android",
+    override val operatingSystemVersion: String? = "8.0.0",
+    override val operatingSystemVersionCode: Int = 26 ,
+    override val numberOfCores: Int = 8,
+    override val internalStorageTotalCapacity: Lazy<Long> = lazy { 10000000L }
+) : Device

9a09bf0dd321a13574f189dd524d1bc2a97b81d6
Lucas <lucaslabari@gmail.com>
2024-03-12 18:10:05 -0300

    Rename test.

--- embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/capture/envelope/log/LogPayloadSourceImplTest.kt
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/capture/envelope/log/LogPayloadSourceImplTest.kt
@@ -21,7 +21,7 @@ internal class LogPayloadSourceImplTest {
     }
 
     @Test
-    fun getLogPayload() {
+    fun `test getLogPayload returns a correct payload`() {
         val payload = impl.getLogPayload()
         val log = checkNotNull(payload.logs?.single())
         assertEquals(1, payload.logs?.size)

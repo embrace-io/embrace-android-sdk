3c9750ff9f3e1744a0eb6b7bdb3978b93a6bf8fb
nelsitoPuglisi <nelson.puglisi@protonmail.com>
2024-03-14 17:07:23 -0300

    unityBuildIdNumber

--- embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/capture/envelope/resource/HostedSdk.kt
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/capture/envelope/resource/HostedSdk.kt
@@ -5,7 +5,7 @@ import io.embrace.android.embracesdk.prefs.PreferencesService
 internal interface HostedSdkInfo {
     val hostedSdkVersion: String?
     val hostedPlatformVersion: String?
-    val buildGuid: String?
+    val unityBuildIdNumber: String?
     val reactNativeBundleId: String?
     val javaScriptPatchNumber: String?
 }
@@ -17,7 +17,7 @@ internal class UnitySdkInfo(
         get() = preferencesService.unitySdkVersionNumber
     override val hostedPlatformVersion: String?
         get() = preferencesService.unityVersionNumber
-    override val buildGuid: String?
+    override val unityBuildIdNumber: String?
         get() = preferencesService.unityBuildIdNumber
     override val reactNativeBundleId: String? = null
     override val javaScriptPatchNumber: String? = null
@@ -30,7 +30,7 @@ internal class ReactNativeSdkInfo(
         get() = preferencesService.rnSdkVersion
     override val hostedPlatformVersion: String?
         get() = preferencesService.reactNativeVersionNumber
-    override val buildGuid: String? = null
+    override val unityBuildIdNumber: String? = null
     override val reactNativeBundleId: String? = preferencesService.javaScriptBundleId
     override val javaScriptPatchNumber: String?
         get() = preferencesService.javaScriptPatchNumber

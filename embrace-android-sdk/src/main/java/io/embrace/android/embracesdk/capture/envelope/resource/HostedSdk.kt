71593a92be67756eb3733fb5d4f90cd84b27946d
nelsitoPuglisi <nelson.puglisi@protonmail.com>
2024-03-15 10:16:39 -0300

    test hosted info internal api

--- embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/capture/envelope/resource/HostedSdk.kt
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/capture/envelope/resource/HostedSdk.kt
@@ -1,9 +1,10 @@
 package io.embrace.android.embracesdk.capture.envelope.resource
 
+import io.embrace.android.embracesdk.logging.InternalEmbraceLogger
 import io.embrace.android.embracesdk.prefs.PreferencesService
 
 internal interface HostedSdkInfo {
-    val hostedSdkVersion: String?
+    var hostedSdkVersion: String?
     val hostedPlatformVersion: String?
     val unityBuildIdNumber: String?
     val reactNativeBundleId: String?
@@ -13,7 +14,7 @@ internal interface HostedSdkInfo {
 internal class UnitySdkInfo(
     private val preferencesService: PreferencesService
 ) : HostedSdkInfo {
-    override val hostedSdkVersion: String?
+    override var hostedSdkVersion: String? = null
         get() = preferencesService.unitySdkVersionNumber
     override val hostedPlatformVersion: String?
         get() = preferencesService.unityVersionNumber
@@ -26,7 +27,7 @@ internal class UnitySdkInfo(
 internal class ReactNativeSdkInfo(
     private val preferencesService: PreferencesService
 ) : HostedSdkInfo {
-    override val hostedSdkVersion: String?
+    override var hostedSdkVersion: String? = null
         get() = preferencesService.rnSdkVersion
     override val hostedPlatformVersion: String?
         get() = preferencesService.reactNativeVersionNumber
@@ -34,4 +35,38 @@ internal class ReactNativeSdkInfo(
     override val reactNativeBundleId: String? = preferencesService.javaScriptBundleId
     override val javaScriptPatchNumber: String?
         get() = preferencesService.javaScriptPatchNumber
+}
+
+internal class FlutterSdkInfo(
+    private val preferencesService: PreferencesService,
+    private val logger: InternalEmbraceLogger
+) : HostedSdkInfo {
+    override var hostedSdkVersion: String? = null
+        get() = field ?: preferencesService.embraceFlutterSdkVersion
+        set(value) {
+            val embraceFlutterSdkVersion = preferencesService.embraceFlutterSdkVersion
+            if (embraceFlutterSdkVersion != null) {
+                logger.logDeveloper("Embrace", "hostedSdkVersion is present")
+                if (value != embraceFlutterSdkVersion) {
+                    logger.logDeveloper("Embrace", "Setting a new hostedSdkVersion")
+                    field = value
+                    preferencesService.embraceFlutterSdkVersion = value
+                }
+            } else {
+                logger.logDeveloper("Embrace", "Setting hostedSdkVersion")
+                field = value
+                preferencesService.embraceFlutterSdkVersion = value
+            }
+        }
+
+    override var hostedPlatformVersion: String? = null
+        get() = field ?: preferencesService.dartSdkVersion
+        set(value) {
+            field = value
+            preferencesService.dartSdkVersion = value
+        }
+
+    override val unityBuildIdNumber: String? = null
+    override val reactNativeBundleId: String? = null
+    override val javaScriptPatchNumber: String? = null
 }

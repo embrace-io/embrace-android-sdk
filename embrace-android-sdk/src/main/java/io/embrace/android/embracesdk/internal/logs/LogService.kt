aae02b211aa5005dbb05bee5555bb922849aa848
Leandro Godon <leandro.godon@embrace.io>
2024-03-19 08:29:53 -0300

    Extract common methods from LogServie and LogMessageService to a common BaseLogService interface.

--- embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/internal/logs/LogService.kt
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/internal/logs/LogService.kt
@@ -3,12 +3,11 @@ package io.embrace.android.embracesdk.internal.logs
 import io.embrace.android.embracesdk.Embrace.AppFramework
 import io.embrace.android.embracesdk.LogExceptionType
 import io.embrace.android.embracesdk.Severity
-import io.embrace.android.embracesdk.session.MemoryCleanerListener
 
 /**
  * Creates log records to be sent using the Open Telemetry Logs data model.
  */
-internal interface LogService : MemoryCleanerListener {
+internal interface LogService : BaseLogService {
 
     /**
      * Creates a log record.
@@ -51,46 +50,4 @@ internal interface LogService : MemoryCleanerListener {
         exceptionName: String?,
         exceptionMessage: String?
     )
-
-    /**
-     * Finds all IDs of log events at info level within the given time window.
-     *
-     * @param startTime the beginning of the time window
-     * @param endTime   the end of the time window
-     * @return the list of log IDs within the specified range
-     */
-    fun findInfoLogIds(startTime: Long, endTime: Long): List<String>
-
-    /**
-     * Finds all IDs of log events at warning level within the given time window.
-     *
-     * @param startTime the beginning of the time window
-     * @param endTime   the end of the time window
-     * @return the list of log IDs within the specified range
-     */
-    fun findWarningLogIds(startTime: Long, endTime: Long): List<String>
-
-    /**
-     * Finds all IDs of log events at error level within the given time window.
-     *
-     * @param startTime the beginning of the time window
-     * @param endTime   the end of the time window
-     * @return the list of log IDs within the specified range
-     */
-    fun findErrorLogIds(startTime: Long, endTime: Long): List<String>
-
-    /**
-     * The total number of info logs that the app attempted to send.
-     */
-    fun getInfoLogsAttemptedToSend(): Int
-
-    /**
-     * The total number of warning logs that the app attempted to send.
-     */
-    fun getWarnLogsAttemptedToSend(): Int
-
-    /**
-     * The total number of error logs that the app attempted to send.
-     */
-    fun getErrorLogsAttemptedToSend(): Int
 }

4001754f480f40307be9e0e4a6f503df92f5e2e9
Hanson Ho <hanson.ho@embrace.io>
2024-04-09 13:31:44 -0700

    Create common attributes object to handle complex schemas (#677)
    ## Goal
    
    Simplify the definition of telemetry schemas to consolidate the appending of session properties, or one-off custom properties, if appropriate.
    
    ## Testing
    
    Add unit tests and leverage existing tests to ensure integration works.

---
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/arch/schema/TelemetryAttributes.kt
@@ -0,0 +1,34 @@
+package io.embrace.android.embracesdk.arch.schema
+
+import io.embrace.android.embracesdk.internal.spans.toEmbraceAttributeName
+import io.embrace.android.embracesdk.session.properties.EmbraceSessionProperties
+import io.opentelemetry.api.common.AttributeKey
+
+/**
+ * Object that aggregates various attributes and returns a [Map] that represents the values at the current state
+ */
+internal class TelemetryAttributes(
+    private val sessionProperties: EmbraceSessionProperties? = null,
+    private val customAttributes: Map<String, String>? = null
+) {
+    private val map: MutableMap<String, String> = mutableMapOf()
+
+    /**
+     * Return a snapshot of the current values of the attributes set on this as a [Map]. Schema keys will always overwrite any previous
+     * entries set on the object.
+     */
+    fun snapshot(): Map<String, String> =
+        (customAttributes ?: emptyMap())
+            .plus(sessionProperties?.get()?.mapKeys { "properties.".toEmbraceAttributeName() + it.key } ?: emptyMap())
+            .plus(map.mapKeys { it.key })
+
+    fun setAttribute(key: EmbraceAttributeKey, value: String) {
+        map[key.name] = value
+    }
+
+    fun setAttribute(key: AttributeKey<String>, value: String) {
+        map[key.key] = value
+    }
+
+    fun getAttribute(key: EmbraceAttributeKey): String? = map[key.name]
+}
---
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/arch/schema/TelemetryAttributesTest.kt
@@ -0,0 +1,93 @@
+package io.embrace.android.embracesdk.arch.schema
+
+import io.embrace.android.embracesdk.fakes.FakeConfigService
+import io.embrace.android.embracesdk.fakes.FakePreferenceService
+import io.embrace.android.embracesdk.internal.utils.Uuid
+import io.embrace.android.embracesdk.logging.InternalEmbraceLogger
+import io.embrace.android.embracesdk.opentelemetry.embSessionId
+import io.embrace.android.embracesdk.opentelemetry.exceptionType
+import io.embrace.android.embracesdk.session.properties.EmbraceSessionProperties
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+internal class TelemetryAttributesTest {
+
+    private lateinit var customAttributes: Map<String, String>
+    private lateinit var sessionProperties: EmbraceSessionProperties
+    private lateinit var telemetryAttributes: TelemetryAttributes
+    private lateinit var sessionId: String
+
+    @Before
+    fun setup() {
+        customAttributes = mapOf("custom" to "attributeValue")
+        sessionProperties = EmbraceSessionProperties(
+            FakePreferenceService(),
+            FakeConfigService(),
+            InternalEmbraceLogger()
+        )
+        sessionId = Uuid.getEmbUuid()
+    }
+
+    @Test
+    fun `only schema properties`() {
+        telemetryAttributes = TelemetryAttributes()
+        telemetryAttributes.setAttribute(embSessionId, sessionId)
+        telemetryAttributes.setAttribute(exceptionType, "exceptionValue")
+        val attributes = telemetryAttributes.snapshot()
+        assertEquals(2, attributes.size)
+        assertEquals(sessionId, attributes[embSessionId.name])
+        assertEquals("exceptionValue", attributes[exceptionType.key])
+    }
+
+    @Test
+    fun `all attributes types`() {
+        telemetryAttributes = TelemetryAttributes(
+            sessionProperties = sessionProperties,
+            customAttributes = customAttributes
+        )
+        telemetryAttributes.setAttribute(embSessionId, sessionId)
+        sessionProperties.add("perm", "permVal", true)
+        sessionProperties.add("temp", "tempVal", false)
+
+        val attributes = telemetryAttributes.snapshot()
+        assertEquals("attributeValue", attributes["custom"])
+        assertEquals("permVal", attributes["emb.properties.perm"])
+        assertEquals("tempVal", attributes["emb.properties.temp"])
+        assertEquals(sessionId, attributes[embSessionId.name])
+        sessionProperties.add("temp", "newVal", false)
+        assertEquals("newVal", telemetryAttributes.snapshot()["emb.properties.temp"])
+    }
+
+    @Test
+    fun `overwritten values returned`() {
+        val newSessionId = Uuid.getEmbUuid()
+        telemetryAttributes = TelemetryAttributes(
+            sessionProperties = sessionProperties
+        )
+        telemetryAttributes.setAttribute(embSessionId, sessionId)
+        telemetryAttributes.setAttribute(embSessionId, newSessionId)
+        sessionProperties.add("perm", "permVal", true)
+        sessionProperties.add("temp", "tempVal", false)
+        sessionProperties.add("perm", "newPermVal", true)
+        sessionProperties.add("temp", "newTempVal", false)
+
+        val attributes = telemetryAttributes.snapshot()
+        assertEquals(3, attributes.size)
+        assertEquals("newPermVal", attributes["emb.properties.perm"])
+        assertEquals("newTempVal", attributes["emb.properties.temp"])
+        assertEquals(newSessionId, attributes[embSessionId.name])
+    }
+
+    @Test
+    fun `schema attribute values take priority if the same key is used`() {
+        val newSessionId = Uuid.getEmbUuid()
+        telemetryAttributes = TelemetryAttributes(
+            customAttributes = mapOf(embSessionId.name to sessionId)
+        )
+        telemetryAttributes.setAttribute(embSessionId, newSessionId)
+        val attributes = telemetryAttributes.snapshot()
+        assertEquals(1, attributes.size)
+        assertEquals(newSessionId, attributes[embSessionId.name])
+    }
+}

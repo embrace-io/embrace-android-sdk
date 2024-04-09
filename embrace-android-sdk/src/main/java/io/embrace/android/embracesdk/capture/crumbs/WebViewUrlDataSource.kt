fcd5f5c79996565e3fbe83908dfc71ce74397d49
Nelsito <nelson.puglisi@protonmail.com>
2024-04-09 10:55:36 -0300

    WebViewUrlDataSource created (#674)

---
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/capture/crumbs/WebViewUrlDataSource.kt
@@ -0,0 +1,65 @@
+package io.embrace.android.embracesdk.capture.crumbs
+
+import io.embrace.android.embracesdk.arch.datasource.DataSourceImpl
+import io.embrace.android.embracesdk.arch.destination.SessionSpanWriter
+import io.embrace.android.embracesdk.arch.destination.SpanEventData
+import io.embrace.android.embracesdk.arch.destination.SpanEventMapper
+import io.embrace.android.embracesdk.arch.limits.UpToLimitStrategy
+import io.embrace.android.embracesdk.arch.schema.SchemaType
+import io.embrace.android.embracesdk.config.behavior.BreadcrumbBehavior
+import io.embrace.android.embracesdk.internal.clock.millisToNanos
+import io.embrace.android.embracesdk.logging.InternalEmbraceLogger
+import io.embrace.android.embracesdk.payload.WebViewBreadcrumb
+
+/**
+ * Captures custom breadcrumbs.
+ */
+internal class WebViewUrlDataSource(
+    private val breadcrumbBehavior: BreadcrumbBehavior,
+    writer: SessionSpanWriter,
+    private val logger: InternalEmbraceLogger
+) : DataSourceImpl<SessionSpanWriter>(
+    destination = writer,
+    logger = logger,
+    limitStrategy = UpToLimitStrategy(logger, breadcrumbBehavior::getWebViewBreadcrumbLimit)
+),
+    SpanEventMapper<WebViewBreadcrumb> {
+
+    companion object {
+        private const val QUERY_PARAMETER_DELIMITER = "?"
+    }
+
+    fun logWebView(url: String?, startTime: Long) {
+        try {
+            alterSessionSpan(
+                inputValidation = {
+                    url != null
+                },
+                captureAction = {
+                    // Check if web view query params should be captured.
+                    var parsedUrl: String = url ?: ""
+                    if (!breadcrumbBehavior.isQueryParamCaptureEnabled()) {
+                        val queryOffset = url?.indexOf(QUERY_PARAMETER_DELIMITER) ?: 0
+                        if (queryOffset > 0) {
+                            parsedUrl = url?.substring(0, queryOffset) ?: ""
+                        }
+                    }
+
+                    val crumb = WebViewBreadcrumb(parsedUrl, startTime)
+                    addEvent(crumb, ::toSpanEventData)
+                }
+            )
+        } catch (ex: Exception) {
+            logger.logError("Failed to log WebView breadcrumb for url $url")
+        }
+    }
+
+    override fun toSpanEventData(obj: WebViewBreadcrumb): SpanEventData {
+        return SpanEventData(
+            SchemaType.WebViewUrl(
+                obj.url
+            ),
+            obj.startTime.millisToNanos()
+        )
+    }
+}
---
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/capture/crumbs/WebViewUrlDataSourceTest.kt
@@ -0,0 +1,126 @@
+package io.embrace.android.embracesdk.capture.crumbs
+
+import io.embrace.android.embracesdk.arch.schema.EmbType
+import io.embrace.android.embracesdk.config.ConfigService
+import io.embrace.android.embracesdk.config.local.SdkLocalConfig
+import io.embrace.android.embracesdk.config.local.WebViewLocalConfig
+import io.embrace.android.embracesdk.fakes.FakeConfigService
+import io.embrace.android.embracesdk.fakes.FakeCurrentSessionSpan
+import io.embrace.android.embracesdk.fakes.fakeBreadcrumbBehavior
+import io.embrace.android.embracesdk.internal.clock.millisToNanos
+import io.embrace.android.embracesdk.logging.InternalEmbraceLogger
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+internal class WebViewUrlDataSourceTest {
+
+    private lateinit var configService: ConfigService
+    private lateinit var source: WebViewUrlDataSource
+    private lateinit var writer: FakeCurrentSessionSpan
+
+    @Before
+    fun setUp() {
+        writer = FakeCurrentSessionSpan()
+    }
+
+    @Test
+    fun `add breadcrumb`() {
+        configService = FakeConfigService(
+            breadcrumbBehavior = fakeBreadcrumbBehavior(
+                localCfg = {
+                    SdkLocalConfig(
+                        webViewConfig = WebViewLocalConfig(
+                            captureWebViews = true,
+                            captureQueryParams = true
+                        )
+                    )
+                }
+            )
+        )
+        source = WebViewUrlDataSource(
+            configService.breadcrumbBehavior,
+            writer,
+            InternalEmbraceLogger(),
+        )
+        source.logWebView(
+            "http://www.google.com?query=123",
+            15000000000
+        )
+        with(writer.addedEvents.single()) {
+            Assert.assertEquals(EmbType.Ux.WebView, schemaType.telemetryType)
+            Assert.assertEquals(15000000000.millisToNanos(), spanStartTimeMs)
+            Assert.assertEquals(
+                mapOf(
+                    EmbType.Ux.WebView.toEmbraceKeyValuePair(),
+                    "webview.url" to "http://www.google.com?query=123"
+                ),
+                schemaType.attributes()
+            )
+        }
+    }
+
+    @Test
+    fun `query param capture disabled`() {
+        configService = FakeConfigService(
+            breadcrumbBehavior = fakeBreadcrumbBehavior(
+                localCfg = {
+                    SdkLocalConfig(
+                        webViewConfig = WebViewLocalConfig(
+                            captureWebViews = true,
+                            captureQueryParams = false
+                        )
+                    )
+                }
+            )
+        )
+        source = WebViewUrlDataSource(
+            configService.breadcrumbBehavior,
+            writer,
+            InternalEmbraceLogger(),
+        )
+        source.logWebView(
+            "http://www.google.com?query=123",
+            15000000000
+        )
+        with(writer.addedEvents.single()) {
+            Assert.assertEquals(EmbType.Ux.WebView, schemaType.telemetryType)
+            Assert.assertEquals(15000000000.millisToNanos(), spanStartTimeMs)
+            Assert.assertEquals(
+                mapOf(
+                    EmbType.Ux.WebView.toEmbraceKeyValuePair(),
+                    "webview.url" to "http://www.google.com"
+                ),
+                schemaType.attributes()
+            )
+        }
+    }
+
+    @Test
+    fun `limit not exceeded`() {
+        configService = FakeConfigService(
+            breadcrumbBehavior = fakeBreadcrumbBehavior(
+                localCfg = {
+                    SdkLocalConfig(
+                        webViewConfig = WebViewLocalConfig(
+                            captureWebViews = true,
+                            captureQueryParams = false
+                        )
+                    )
+                }
+            )
+        )
+        source = WebViewUrlDataSource(
+            configService.breadcrumbBehavior,
+            writer,
+            InternalEmbraceLogger(),
+        )
+        repeat(150) { k ->
+            source.logWebView(
+                "http://www.google.com?query=$k",
+                15000000000
+            )
+        }
+        Assert.assertEquals(100, writer.addedEvents.size)
+    }
+}

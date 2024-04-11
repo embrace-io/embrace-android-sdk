97ef624210cb0a2d93b9b9ca0516e8db7800b482
bidetofevil <hanson.ho@embrace.io>
2024-04-11 09:28:40 -0700

    Beef up functional tests

--- embrace-android-sdk/src/integrationTest/kotlin/io/embrace/android/embracesdk/features/SessionPropertiesTest.kt
+++ embrace-android-sdk/src/integrationTest/kotlin/io/embrace/android/embracesdk/features/SessionPropertiesTest.kt
@@ -6,6 +6,7 @@ import io.embrace.android.embracesdk.arch.schema.EmbType
 import io.embrace.android.embracesdk.internal.spans.hasFixedAttribute
 import io.embrace.android.embracesdk.recordSession
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,15 +22,34 @@ internal class SessionPropertiesTest {
     fun `session properties`() {
         with(testRule) {
             embrace.addSessionProperty("always", "thurr", true)
-            val session = checkNotNull(harness.recordSession {
+            val session1 = checkNotNull(harness.recordSession {
                 embrace.addSessionProperty("perm", "permVal", true)
                 embrace.addSessionProperty("temp", "tempVal", false)
             })
 
-            val sessionSpan = checkNotNull(session.spans?.find { it.hasFixedAttribute(EmbType.Ux.Session) })
-            assertEquals("thurr", sessionSpan.attributes["always"])
-            assertEquals("permVal", sessionSpan.attributes["perm"])
-            assertEquals("tempVal", sessionSpan.attributes["temp"])
+            with(checkNotNull(session1.spans?.find { it.hasFixedAttribute(EmbType.Ux.Session) })) {
+                assertEquals("thurr", attributes["always"])
+                assertEquals("permVal", attributes["perm"])
+                assertEquals("tempVal", attributes["temp"])
+            }
+
+            val session2 = checkNotNull(harness.recordSession {
+                embrace.addSessionProperty("newTemp", "value", false)
+                embrace.removeSessionProperty("perm")
+            })
+
+            with(checkNotNull(session2.spans?.find { it.hasFixedAttribute(EmbType.Ux.Session) })) {
+                assertEquals("thurr", attributes["always"])
+                assertEquals("value", attributes["newTemp"])
+                assertNull(attributes["perm"])
+                assertNull(attributes["temp"])
+            }
+
+            val attributesFromSdk = checkNotNull(embrace.getSessionProperties())
+            assertEquals("thurr", attributesFromSdk["always"])
+            assertNull(attributesFromSdk["perm"])
+            assertNull(attributesFromSdk["temp"])
+            assertNull(attributesFromSdk["newTemp"])
         }
     }
 }

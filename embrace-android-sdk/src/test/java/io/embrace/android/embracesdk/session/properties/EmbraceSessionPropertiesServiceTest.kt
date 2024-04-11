217fd1b1cf65a4a464bd6e19c8ddb5b991546acf
bidetofevil <hanson.ho@embrace.io>
2024-04-11 08:48:40 -0700

    Fix after rebase

--- embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/session/properties/EmbraceSessionPropertiesServiceTest.kt
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/session/properties/EmbraceSessionPropertiesServiceTest.kt
@@ -32,7 +32,7 @@ internal class EmbraceSessionPropertiesServiceTest {
             writer = fakeCurrentSessionSpan,
             logger = logger,
         )
-        service = EmbraceSessionPropertiesService(ndkService, props, dataSource)
+        service = EmbraceSessionPropertiesService(ndkService, props) { dataSource }
     }
 
     @Test

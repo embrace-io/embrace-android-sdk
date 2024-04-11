53b607f8f5e385f5b8b28a21caa4dd501b346415
bidetofevil <hanson.ho@embrace.io>
2024-04-10 23:12:54 -0700

    Hook up properties data source to service

--- embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/session/properties/EmbraceSessionPropertiesService.kt
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/session/properties/EmbraceSessionPropertiesService.kt
@@ -1,15 +1,20 @@
 package io.embrace.android.embracesdk.session.properties
 
+import io.embrace.android.embracesdk.capture.session.SessionPropertiesDataSource
 import io.embrace.android.embracesdk.ndk.NdkService
 
 internal class EmbraceSessionPropertiesService(
     private val ndkService: NdkService,
-    private val sessionProperties: EmbraceSessionProperties
+    private val sessionProperties: EmbraceSessionProperties,
+    private val sessionPropertiesDataSource: SessionPropertiesDataSource?
 ) : SessionPropertiesService {
 
     override fun addProperty(key: String, value: String, permanent: Boolean): Boolean {
         val added = sessionProperties.add(key, value, permanent)
         if (added) {
+            sessionPropertiesDataSource?.apply {
+                addProperty(key, value)
+            }
             ndkService.onSessionPropertiesUpdate(sessionProperties.get())
         }
         return added
@@ -18,6 +23,9 @@ internal class EmbraceSessionPropertiesService(
     override fun removeProperty(key: String): Boolean {
         val removed = sessionProperties.remove(key)
         if (removed) {
+            sessionPropertiesDataSource?.apply {
+                removeProperty(key)
+            }
             ndkService.onSessionPropertiesUpdate(sessionProperties.get())
         }
         return removed

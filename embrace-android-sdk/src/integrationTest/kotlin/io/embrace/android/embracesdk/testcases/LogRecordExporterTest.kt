b614e2f6fa7de4f12946eabf7c1e8715603d7760
nelsitoPuglisi <nelson.puglisi@protonmail.com>
2024-04-12 13:31:11 -0300

    integration test

---
+++ embrace-android-sdk/src/integrationTest/kotlin/io/embrace/android/embracesdk/testcases/LogRecordExporterTest.kt
@@ -0,0 +1,49 @@
+package io.embrace.android.embracesdk.testcases
+
+import android.os.Build
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.embrace.android.embracesdk.IntegrationTestRule
+import io.embrace.android.embracesdk.Severity
+import io.embrace.android.embracesdk.config.remote.RemoteConfig
+import io.embrace.android.embracesdk.config.remote.SessionRemoteConfig
+import io.embrace.android.embracesdk.fakes.FakeConfigService
+import io.embrace.android.embracesdk.fakes.FakeLogRecordExporter
+import io.embrace.android.embracesdk.fakes.fakeSessionBehavior
+import io.embrace.android.embracesdk.recordSession
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import java.lang.Thread.sleep
+
+@Config(sdk = [Build.VERSION_CODES.TIRAMISU])
+@RunWith(AndroidJUnit4::class)
+internal class LogRecordExporterTest {
+
+    @Rule
+    @JvmField
+    val testRule: IntegrationTestRule = IntegrationTestRule(
+        harnessSupplier = {
+            IntegrationTestRule.newHarness(startImmediately = false)
+        }
+    )
+
+    @Test
+    fun `SDK can receive a LogRecordExporter`() {
+        with(testRule) {
+
+            val fakeLogRecordExporter = FakeLogRecordExporter()
+            embrace.addLogRecordExporter(fakeLogRecordExporter)
+            embrace.start(harness.fakeCoreModule.context)
+
+            harness.recordSession {
+                embrace.logMessage("test message", Severity.INFO)
+
+                sleep(3000)
+            }
+            Assert.assertTrue((fakeLogRecordExporter.exportedLogs?.size ?: 0) > 0)
+            Assert.assertEquals("test message", fakeLogRecordExporter.exportedLogs?.first()?.body)
+        }
+    }
+}

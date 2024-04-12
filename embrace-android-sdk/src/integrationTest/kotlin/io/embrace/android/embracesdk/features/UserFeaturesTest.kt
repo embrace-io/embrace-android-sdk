671bef20e74128116c7258aa1eb508cebec2a210
bidetofevil <hanson.ho@embrace.io>
2024-04-11 23:25:43 -0700

    Create integration tests for v1 payload for user info

---
+++ embrace-android-sdk/src/integrationTest/kotlin/io/embrace/android/embracesdk/features/UserFeaturesTest.kt
@@ -0,0 +1,62 @@
+package io.embrace.android.embracesdk.features
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.embrace.android.embracesdk.IntegrationTestRule
+import io.embrace.android.embracesdk.payload.SessionMessage
+import io.embrace.android.embracesdk.recordSession
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+internal class UserFeaturesTest {
+
+    @Rule
+    @JvmField
+    val testRule: IntegrationTestRule = IntegrationTestRule(
+        harnessSupplier = {
+            IntegrationTestRule.newHarness(startImmediately = false)
+        }
+    )
+
+    @Test
+    fun `user info setting and clearing`() {
+        with(testRule) {
+            harness.androidServicesModule.preferencesService.userIdentifier = "customId"
+            harness.androidServicesModule.preferencesService.username = "customUserName"
+            harness.androidServicesModule.preferencesService.userEmailAddress = "custom@domain.com"
+            embrace.start(harness.fakeCoreModule.context)
+            with(checkNotNull(harness.recordSession { })) {
+                assertUserInfo("customId", "customUserName", "custom@domain.com")
+            }
+
+            val session = harness.recordSession {
+                embrace.clearUserIdentifier()
+                embrace.clearUsername()
+                embrace.clearUserEmail()
+            }
+
+            with(checkNotNull(session)) {
+                assertUserInfo(null, null, null)
+            }
+
+            embrace.setUserIdentifier("newId")
+            embrace.setUsername("newUserName")
+            embrace.setUserEmail("new@domain.com")
+
+            with(checkNotNull(harness.recordSession { })) {
+                assertUserInfo("newId", "newUserName", "new@domain.com")
+            }
+        }
+    }
+
+    private fun SessionMessage.assertUserInfo(userId: String?, userName: String?, email: String?) {
+        assertEquals(userId, userInfo?.userId)
+        assertEquals(userId, testRule.harness.androidServicesModule.preferencesService.userIdentifier)
+        assertEquals(userName, userInfo?.username)
+        assertEquals(userName, testRule.harness.androidServicesModule.preferencesService.username)
+        assertEquals(email, userInfo?.email)
+        assertEquals(email, testRule.harness.androidServicesModule.preferencesService.userEmailAddress)
+    }
+}

f66a257327b7e00b72c58c9c0c8bd7ed83a86ba7
Jamie Lynch <fractalwrench@gmail.com>
2024-04-24 15:23:00 +0100

    remove unused test code

--- embrace-android-sdk/src/androidTest/java/io/embrace/android/embracesdk/RequestValidator.kt
+++
@@ -1,8 +0,0 @@
-package io.embrace.android.embracesdk
-
-import okhttp3.mockwebserver.RecordedRequest
-
-internal class RequestValidator(
-    val endpoint: EmbraceEndpoint,
-    val validate: (response: RecordedRequest) -> Unit
-)

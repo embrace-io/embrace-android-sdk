807fc0502986f98f3eff8ab0e2e88661b0810b0f
Leandro Godon <leandro.godon@embrace.io>
2024-04-29 08:52:04 -0300

    Filter out emb.private logs from external exporters.

--- embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/internal/logs/EmbraceLogRecordExporter.kt
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/internal/logs/EmbraceLogRecordExporter.kt
@@ -1,5 +1,7 @@
 package io.embrace.android.embracesdk.internal.logs
 
+import io.embrace.android.embracesdk.arch.schema.PrivateSpan
+import io.embrace.android.embracesdk.internal.spans.hasFixedAttribute
 import io.opentelemetry.sdk.common.CompletableResultCode
 import io.opentelemetry.sdk.logs.data.LogRecordData
 import io.opentelemetry.sdk.logs.export.LogRecordExporter
@@ -7,9 +9,22 @@ import io.opentelemetry.sdk.logs.export.LogRecordExporter
 /**
  * Exports the given [LogRecordData] to a [LogSink]
  */
-internal class EmbraceLogRecordExporter(private val logSink: LogSink) : LogRecordExporter {
-    override fun export(logs: Collection<LogRecordData>): CompletableResultCode =
-        logSink.storeLogs(logs.toList())
+internal class EmbraceLogRecordExporter(
+    private val logSink: LogSink,
+    private val externalLogRecordExporter: LogRecordExporter
+) : LogRecordExporter {
+    override fun export(logs: Collection<LogRecordData>): CompletableResultCode {
+        val result = logSink.storeLogs(logs.toList())
+        if (result == CompletableResultCode.ofSuccess()) {
+            return externalLogRecordExporter.export(
+                logs.filterNot {
+                    it.hasFixedAttribute(PrivateSpan)
+                }
+            )
+        }
+
+        return result
+    }
 
     override fun flush(): CompletableResultCode = CompletableResultCode.ofSuccess()
 

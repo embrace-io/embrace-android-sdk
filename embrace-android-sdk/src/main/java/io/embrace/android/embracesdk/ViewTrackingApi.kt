97ae77a9dbc59d31ee7760c1902688151b654598
Jamie Lynch <fractalwrench@gmail.com>
2024-05-17 14:47:43 +0100

    refactor: extract view tracking delegate

---
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/ViewTrackingApi.kt
@@ -0,0 +1,70 @@
+package io.embrace.android.embracesdk
+
+import android.app.Application
+import io.embrace.android.embracesdk.payload.TapBreadcrumb
+
+internal interface ViewTrackingApi {
+
+    /**
+     * Register ComposeActivityListener as Activity Lifecycle Callbacks into the Application
+     *
+     * @param app Global application class
+     */
+    fun registerComposeActivityListener(app: Application)
+
+    /**
+     * Register ComposeActivityListener as Activity Lifecycle Callbacks into the Application
+     *
+     * @param app Global application class
+     */
+    fun unregisterComposeActivityListener(app: Application)
+
+    /**
+     * Log the start of a view.
+     *
+     * A matching call to endView must be made.
+     *
+     * @param name the name of the view to log
+     */
+    fun startView(name: String): Boolean
+
+    /**
+     * Log the end of a view.
+     *
+     * A matching call to startView must be made before this is called.
+     *
+     * @param name the name of the view to log
+     */
+    fun endView(name: String): Boolean
+
+    /**
+     * Logs a tap on a screen element.
+     *
+     * @param point       the coordinates of the screen tap
+     * @param elementName the name of the element which was tapped
+     * @param type        the type of tap that occurred
+     */
+    fun logTap(point: Pair<Float?, Float?>, elementName: String, type: TapBreadcrumb.TapBreadcrumbType)
+
+    /**
+     * Logs a React Native Redux Action.
+     */
+    fun logRnAction(
+        name: String,
+        startTime: Long,
+        endTime: Long,
+        properties: Map<String?, Any?>,
+        bytesSent: Int,
+        output: String
+    )
+
+    /**
+     * Logs the fact that a particular view was entered.
+     *
+     * If the previously logged view has the same name, a duplicate view breadcrumb will not be
+     * logged.
+     *
+     * @param screen the name of the view to log
+     */
+    fun logRnView(screen: String)
+}

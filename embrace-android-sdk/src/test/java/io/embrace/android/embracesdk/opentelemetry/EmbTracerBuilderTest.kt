8389991ce41a6796b17b3e39b4ef5be0de6b344c
bidetofevil <hanson.ho@embrace.io>
2024-05-29 21:53:36 -0700

    Implementation for Tracer, TracerBuilder, and TracerProvider OTel APIs

---
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/opentelemetry/EmbTracerBuilderTest.kt
@@ -0,0 +1,22 @@
+package io.embrace.android.embracesdk.opentelemetry
+
+import io.embrace.android.embracesdk.fakes.FakeTracer
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+internal class EmbTracerBuilderTest {
+
+    @Test
+    fun `check tracer attributes from builder`() {
+        val expectedKey = TracerKey("foo", "v1", "url")
+        val embTracerBuilder = EmbTracerBuilder(
+            instrumentationScopeName = "foo",
+            tracerSupplier = ::createTracer
+        )
+
+        val tracer = embTracerBuilder.setSchemaUrl("url").setInstrumentationVersion("v1").build() as FakeTracer
+        assertEquals(expectedKey, tracer.tracerKey)
+    }
+
+    private fun createTracer(key: TracerKey): FakeTracer = FakeTracer(tracerKey = key)
+}

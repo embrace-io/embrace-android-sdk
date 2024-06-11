d0e16e0e63c17046b768c9a7ff8988b6f28da35f
Francisco Prieto <priettt@gmail.com>
2024-06-11 11:54:40 -0300

    Remove unused LegacyExceptionError and LegacyExceptionErrorInfo

--- embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/fakes/FakeInternalErrorService.kt
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/fakes/FakeInternalErrorService.kt
@@ -3,14 +3,11 @@ package io.embrace.android.embracesdk.fakes
 import io.embrace.android.embracesdk.capture.internal.errors.InternalErrorDataSource
 import io.embrace.android.embracesdk.capture.internal.errors.InternalErrorService
 import io.embrace.android.embracesdk.internal.utils.Provider
-import io.embrace.android.embracesdk.payload.LegacyExceptionError
 
 internal class FakeInternalErrorService : InternalErrorService {
 
     override var internalErrorDataSource: Provider<InternalErrorDataSource?> = { null }
     var throwables: MutableList<Throwable> = mutableListOf()
-    var resetCallCount: Int = 0
-    var data: LegacyExceptionError? = null
 
     override fun handleInternalError(throwable: Throwable) {
         throwables.add(throwable)

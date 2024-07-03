158cc5a955f9efa1a35b06db885e225770328f1b
Francisco Prieto <priettt@gmail.com>
2024-07-03 11:36:11 -0300

    Added @JsonClass(generateAdapter = false) to enums
    https://github.com/square/moshi/tree/master?tab=readme-ov-file#enums

--- embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/capture/metadata/AppEnvironment.kt
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/capture/metadata/AppEnvironment.kt
@@ -2,12 +2,14 @@ package io.embrace.android.embracesdk.capture.metadata
 
 import android.content.pm.ApplicationInfo
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
 internal class AppEnvironment(appInfo: ApplicationInfo) {
     val isDebug: Boolean = with(appInfo) { flags and ApplicationInfo.FLAG_DEBUGGABLE != 0 }
 
     val environment: Environment = if (isDebug) Environment.DEV else Environment.PROD
 
+    @JsonClass(generateAdapter = false)
     internal enum class Environment(val value: String) {
         @Json(name = "dev")
         DEV("dev"),
--- embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/payload/LifeEventType.kt
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/payload/LifeEventType.kt
@@ -1,10 +1,12 @@
 package io.embrace.android.embracesdk.payload
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
 /**
  * Enum to discriminate the different ways a session can start / end
  */
+@JsonClass(generateAdapter = false)
 internal enum class LifeEventType {
 
     /* Session values */

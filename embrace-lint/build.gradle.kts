a06e6d271d978b46295a5cf3727e8d43c313a205
bidetofevil <hanson.ho@embrace.io>
2024-07-09 15:53:38 -0700

    Stop using jvmToolchain

--- embrace-lint/build.gradle.kts
+++ embrace-lint/build.gradle.kts
@@ -1,11 +1,22 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("java-library")
     id("kotlin")
     id("com.android.lint")
 }
 
-kotlin {
-    jvmToolchain(8)
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+project.tasks.withType(KotlinCompile::class.java).configureEach {
+    kotlinOptions {
+        apiVersion = "1.7"
+        languageVersion = "1.7"
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
 }
 
 dependencies {
--- embrace-test-common/build.gradle.kts
+++ embrace-test-common/build.gradle.kts
@@ -1,8 +1,19 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("java-library")
     id("kotlin")
 }
 
-kotlin {
-    jvmToolchain(8)
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+project.tasks.withType(KotlinCompile::class.java).configureEach {
+    kotlinOptions {
+        apiVersion = "1.7"
+        languageVersion = "1.7"
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
 }

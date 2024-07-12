49e7f0832ded5d92414a0300d5976c0853ade93f
Jamie Lynch <fractalwrench@gmail.com>
2024-07-12 14:28:06 +0100

    build: setup correct dependencies for core module

--- embrace-android-core/build.gradle.kts
+++ embrace-android-core/build.gradle.kts
@@ -7,3 +7,12 @@ description = "Embrace Android SDK: Core"
 android {
     namespace = "io.embrace.android.embracesdk.core"
 }
+
+dependencies {
+    compileOnly(platform(libs.opentelemetry.bom))
+    compileOnly(libs.opentelemetry.api)
+    compileOnly(libs.opentelemetry.sdk)
+    compileOnly(libs.opentelemetry.context)
+    compileOnly(libs.opentelemetry.semconv)
+    compileOnly(libs.opentelemetry.semconv.incubating)
+}

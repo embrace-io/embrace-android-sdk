4e20cfa3a4d2e4ca60bfe5839dbe79d78e90002c
Jamie Lynch <fractalwrench@gmail.com>
2024-07-17 15:26:05 +0100

    build: setup api module

---
+++ embrace-android-api/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("embrace-defaults")
+}
+
+description = "Embrace Android SDK: API"
+
+android {
+    namespace = "io.embrace.android.embracesdk.api"
+}
+
+dependencies {
+    compileOnly(platform(libs.opentelemetry.bom))
+    compileOnly(libs.opentelemetry.api)
+}

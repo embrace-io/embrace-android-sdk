4509885450adf82a2012f22bc8fc1351d7fdb103
Jamie Lynch <fractalwrench@gmail.com>
2024-08-02 11:07:18 +0100

    fix tests

--- embrace-android-core/src/main/kotlin/io/embrace/android/embracesdk/internal/opentelemetry/TracerKey.kt
+++ embrace-android-core/src/main/kotlin/io/embrace/android/embracesdk/internal/opentelemetry/TracerKey.kt
@@ -1,6 +1,6 @@
 package io.embrace.android.embracesdk.internal.opentelemetry
 
-internal data class TracerKey(
+public data class TracerKey(
     val instrumentationScopeName: String,
     var instrumentationScopeVersion: String? = null,
     var schemaUrl: String? = null

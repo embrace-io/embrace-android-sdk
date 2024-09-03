c542a9ce482bf12d8c569d3af9ac8fc1c3cd1b7f
Jamie Lynch <fractalwrench@gmail.com>
2024-09-03 10:40:02 +0100

    perf: avoid thread contention in otel init

--- embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/injection/ModuleInitBootstrapperTest.kt
+++ embrace-android-sdk/src/test/java/io/embrace/android/embracesdk/injection/ModuleInitBootstrapperTest.kt
@@ -2,27 +2,20 @@ package io.embrace.android.embracesdk.injection
 
 import android.content.Context
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import io.embrace.android.embracesdk.fakes.FakeClock
 import io.embrace.android.embracesdk.fakes.FakeNativeFeatureModule
 import io.embrace.android.embracesdk.fakes.injection.FakeCoreModule
-import io.embrace.android.embracesdk.fakes.injection.FakeInitModule
-import io.embrace.android.embracesdk.fakes.injection.FakeWorkerThreadModule
 import io.embrace.android.embracesdk.internal.injection.EssentialServiceModuleImpl
 import io.embrace.android.embracesdk.internal.injection.ModuleInitBootstrapper
 import io.embrace.android.embracesdk.internal.logging.EmbLogger
 import io.embrace.android.embracesdk.internal.logging.EmbLoggerImpl
 import io.embrace.android.embracesdk.internal.payload.AppFramework
-import io.embrace.android.embracesdk.internal.worker.WorkerName
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNotNull
-import org.junit.Assert.assertThrows
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RuntimeEnvironment
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.TimeoutException
 
 @RunWith(AndroidJUnit4::class)
 internal class ModuleInitBootstrapperTest {
@@ -91,7 +84,7 @@ internal class ModuleInitBootstrapperTest {
     }
 
     @Test
-    fun `async init returns normally and without failure`() {
+    fun `init returns normally and without failure`() {
         assertTrue(
             moduleInitBootstrapper.init(
                 context = context,
@@ -99,34 +92,6 @@ internal class ModuleInitBootstrapperTest {
                 sdkStartTimeMs = 0L,
             )
         )
-        moduleInitBootstrapper.waitForAsyncInit()
-    }
-
-    @Test
-    fun `async init throws exception if it waiting for too long`() {
-        val fakeInitModule = FakeInitModule(clock = FakeClock())
-        val fakeCoreModule = FakeCoreModule(logger = logger)
-        val fakeWorkerThreadModule = FakeWorkerThreadModule(
-            fakeInitModule = fakeInitModule,
-            name = WorkerName.BACKGROUND_REGISTRATION
-        )
-        val bootstrapper = ModuleInitBootstrapper(
-            initModule = fakeInitModule,
-            coreModuleSupplier = { _, _ -> fakeCoreModule },
-            workerThreadModuleSupplier = { _ -> fakeWorkerThreadModule },
-            nativeFeatureModuleSupplier = { _, _, _, _, _, _, _, _, _ -> FakeNativeFeatureModule() },
-            logger = EmbLoggerImpl()
-        )
-        assertTrue(
-            bootstrapper.init(
-                context = context,
-                appFramework = AppFramework.NATIVE,
-                sdkStartTimeMs = 0L,
-            )
-        )
-        assertThrows(TimeoutException::class.java) {
-            bootstrapper.waitForAsyncInit(500L, TimeUnit.MILLISECONDS)
-        }
     }
 
     @Test

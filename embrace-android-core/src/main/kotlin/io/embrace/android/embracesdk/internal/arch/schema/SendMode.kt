628dfcb9b8f0599559305eb8315947e8303adc74
bidetofevil <hanson.ho@embrace.io>
2024-09-05 08:30:42 -0700

    Add Kdocs to SendMode enum

--- embrace-android-core/src/main/kotlin/io/embrace/android/embracesdk/internal/arch/schema/SendMode.kt
+++ embrace-android-core/src/main/kotlin/io/embrace/android/embracesdk/internal/arch/schema/SendMode.kt
@@ -4,8 +4,24 @@ import io.embrace.android.embracesdk.internal.arch.schema.SendMode.DEFAULT
 import io.embrace.android.embracesdk.internal.arch.schema.SendMode.DEFER
 import io.embrace.android.embracesdk.internal.arch.schema.SendMode.IMMEDIATE
 
+/**
+ * How a given payload should be delivered to the Embrace server
+ */
 public enum class SendMode {
-    DEFAULT, IMMEDIATE, DEFER
+    /**
+     * Use the default delivery semantics - no customization required
+     */
+    DEFAULT,
+
+    /**
+     * If supported for that signal/payload type, deliver this as soon as possible and do not batch
+     */
+    IMMEDIATE,
+
+    /**
+     * Queue for delivery at the next convenient time. Used when the delivery environment is unstable, e.g. when an app is about to crash.
+     */
+    DEFER
 }
 
 public fun String.toSendMode(): SendMode {

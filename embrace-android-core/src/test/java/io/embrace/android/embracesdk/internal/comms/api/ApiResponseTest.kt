936745391a68c762f0146c65712924e6ae206e76
Jamie Lynch <fractalwrench@gmail.com>
2024-09-06 11:39:27 +0100

    fix: retry session delivery where possible

---
+++ embrace-android-core/src/test/java/io/embrace/android/embracesdk/internal/comms/api/ApiResponseTest.kt
@@ -0,0 +1,21 @@
+package io.embrace.android.embracesdk.internal.comms.api
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+internal class ApiResponseTest {
+
+    @Test
+    fun `test retry logic`() {
+        assertFalse(ApiResponse.Success(null, null).shouldRetry)
+        assertFalse(ApiResponse.NotModified.shouldRetry)
+        assertFalse(ApiResponse.PayloadTooLarge.shouldRetry)
+        assertFalse(ApiResponse.Failure(400, null).shouldRetry)
+
+        assertTrue(ApiResponse.TooManyRequests(Endpoint.EVENTS, null).shouldRetry)
+        assertTrue(ApiResponse.Incomplete(RuntimeException()).shouldRetry)
+        assertTrue(ApiResponse.None.shouldRetry)
+        assertTrue(ApiResponse.Failure(500, null).shouldRetry)
+    }
+}

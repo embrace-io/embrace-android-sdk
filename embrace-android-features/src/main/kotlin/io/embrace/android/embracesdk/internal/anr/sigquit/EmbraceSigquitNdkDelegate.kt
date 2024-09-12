4b3c86b9983f17e32f77c6ffd6f3e8c0751c4459
Jamie Lynch <fractalwrench@gmail.com>
2024-09-12 16:55:49 +0100

    fix: prevent jni error on sigquit with anr capture enabled (#1355)

--- embrace-android-features/src/main/kotlin/io/embrace/android/embracesdk/internal/anr/sigquit/EmbraceSigquitNdkDelegate.kt
+++ embrace-android-features/src/main/kotlin/io/embrace/android/embracesdk/internal/anr/sigquit/EmbraceSigquitNdkDelegate.kt
@@ -2,9 +2,12 @@ package io.embrace.android.embracesdk.internal.anr.sigquit
 
 // IMPORTANT: This class is referenced by emb_anr_manager.c. Move or rename both at the same time, or it will break.
 class EmbraceSigquitNdkDelegate : SigquitNdkDelegate {
-    external override fun installGoogleAnrHandler(googleThreadId: Int): Int
+    external override fun installGoogleAnrHandler(
+        googleThreadId: Int,
+        instance: SigquitDataSource
+    ): Int
 }
 
 internal interface SigquitNdkDelegate {
-    fun installGoogleAnrHandler(googleThreadId: Int): Int
+    fun installGoogleAnrHandler(googleThreadId: Int, instance: SigquitDataSource): Int
 }

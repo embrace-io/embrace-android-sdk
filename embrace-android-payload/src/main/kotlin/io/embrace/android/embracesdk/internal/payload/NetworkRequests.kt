e6ceafc8cd203833a31299a12c253a0b4210f17c
Jamie Lynch <fractalwrench@gmail.com>
2024-10-16 16:44:32 +0100

    remove unnecessary json adapters

--- embrace-android-payload/src/main/kotlin/io/embrace/android/embracesdk/internal/payload/NetworkRequests.kt
+++
@@ -1,9 +0,0 @@
-package io.embrace.android.embracesdk.internal.payload
-
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class NetworkRequests(
-    @Json(name = "v2") val networkSessionV2: NetworkSessionV2?
-)

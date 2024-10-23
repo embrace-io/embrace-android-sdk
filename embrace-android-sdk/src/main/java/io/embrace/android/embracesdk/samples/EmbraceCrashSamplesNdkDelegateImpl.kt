41e40692c89e04142e5eb47c40aaa7177ffe73da
Jamie Lynch <fractalwrench@gmail.com>
2024-10-23 16:54:24 +0100

    refactor: remove sample code

--- embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/samples/EmbraceCrashSamplesNdkDelegateImpl.kt
+++
@@ -1,9 +0,0 @@
-package io.embrace.android.embracesdk.samples
-
-internal class EmbraceCrashSamplesNdkDelegateImpl : CrashSamplesNdkDelegate {
-    external override fun sigIllegalInstruction()
-    external override fun throwException()
-    external override fun sigAbort()
-    external override fun sigfpe()
-    external override fun sigsegv()
-}

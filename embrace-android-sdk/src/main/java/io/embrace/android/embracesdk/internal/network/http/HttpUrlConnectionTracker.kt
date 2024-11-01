34245229bda205c5f501745439eecc3a63f99f4f
bidetofevil <hanson.ho@embrace.io>
2024-11-01 11:17:35 -0700

    Disable HttpUrlConnection instrumention in unit tests

--- embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/internal/network/http/HttpUrlConnectionTracker.kt
+++ embrace-android-sdk/src/main/java/io/embrace/android/embracesdk/internal/network/http/HttpUrlConnectionTracker.kt
@@ -1,7 +1,6 @@
 package io.embrace.android.embracesdk.internal.network.http
 
 internal object HttpUrlConnectionTracker {
-    @JvmStatic
     fun registerFactory(requestContentLengthCaptureEnabled: Boolean) {
         StreamHandlerFactoryInstaller.registerFactory(requestContentLengthCaptureEnabled)
     }
--- embrace-test-fakes/src/main/kotlin/io/embrace/android/embracesdk/fakes/behavior/FakeNetworkBehavior.kt
+++ embrace-test-fakes/src/main/kotlin/io/embrace/android/embracesdk/fakes/behavior/FakeNetworkBehavior.kt
@@ -7,10 +7,11 @@ import io.embrace.android.embracesdk.internal.config.remote.NetworkCaptureRuleRe
 class FakeNetworkBehavior(
     private val captureLimit: Int = 1000,
     private val domains: Map<String, Int> = emptyMap(),
+    private val captureHttpUrlConnectionRequests: Boolean = true,
 ) : NetworkBehavior {
     override fun getTraceIdHeader(): String = NetworkCaptureConfig.CONFIG_TRACE_ID_HEADER_DEFAULT_VALUE
     override fun isRequestContentLengthCaptureEnabled(): Boolean = false
-    override fun isHttpUrlConnectionCaptureEnabled(): Boolean = true
+    override fun isHttpUrlConnectionCaptureEnabled(): Boolean = captureHttpUrlConnectionRequests
     override fun getLimitsByDomain(): Map<String, Int> = domains
     override fun getRequestLimitPerDomain(): Int = captureLimit
     override fun isUrlEnabled(url: String): Boolean = true

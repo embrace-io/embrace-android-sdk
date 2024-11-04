36da0e09f5a41ce20c21e75017b1754eff6dd4f7
Jamie Lynch <fractalwrench@gmail.com>
2024-11-04 11:05:40 +0000

    add example list

--- examples/ExampleApp/app/src/main/kotlin/io/embrace/android/exampleapp/MainActivity.kt
+++ examples/ExampleApp/app/src/main/kotlin/io/embrace/android/exampleapp/MainActivity.kt
@@ -4,44 +4,55 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import io.embrace.android.exampleapp.ui.screens.ExampleCodeList
+import io.embrace.android.exampleapp.ui.screens.codeExamples
 import io.embrace.android.exampleapp.ui.theme.ExampleAppTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ExampleAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            ExampleCodeListScreen()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ExampleCodeListScreen() {
+    ExampleAppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar(
+                    title = { Text(stringResource(R.string.app_name)) },
+                )
+            },
+            content = { paddingValues ->
+                Column(modifier = Modifier.padding(paddingValues)) {
+                    ExampleCodeList(
+                        buttonItems = codeExamples
+                    )
+                }
+            })
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ExampleAppTheme {
-        Greeting("Android")
-    }
+fun ExampleCodeListScreenPreview() {
+    ExampleCodeListScreen()
 }

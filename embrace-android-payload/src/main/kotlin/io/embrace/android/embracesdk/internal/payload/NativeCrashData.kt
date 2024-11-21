8d5b9a5fdb2602d2f0e5e3425f9c0e5fb6642c5c
Jamie Lynch <fractalwrench@gmail.com>
2024-11-21 11:26:43 +0000

    refactor: remove unused class

--- embrace-android-payload/src/main/kotlin/io/embrace/android/embracesdk/internal/payload/NativeCrashData.kt
+++ embrace-android-payload/src/main/kotlin/io/embrace/android/embracesdk/internal/payload/NativeCrashData.kt
@@ -15,8 +15,4 @@ class NativeCrashData(
     @Json(name = "symbols") var symbols: Map<String?, String?>?,
     @Json(name = "errors") var errors: List<NativeCrashDataError?>?,
     @Json(name = "map") var map: String?,
-) {
-
-    fun getCrash(crashNumber: Int): NativeCrash =
-        NativeCrash(nativeCrashId, crash, symbols, errors, unwindError, map, crashNumber)
-}
+)

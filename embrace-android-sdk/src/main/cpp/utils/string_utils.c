99038bb49994e44c3908bbf8ce32915a8d152c60
Francisco Prieto <priettt@gmail.com>
2025-01-02 12:12:39 -0300

    null-terminate destination buffer in emb_strncpy

--- embrace-android-sdk/src/main/cpp/utils/string_utils.c
+++ embrace-android-sdk/src/main/cpp/utils/string_utils.c
@@ -3,23 +3,22 @@
 
 /**
  * Copy a string from source to destination. If the source is larger than the destination, it will be truncated.
+ * The destination buffer will always be null-terminated.
  * @param destination The destination buffer.
  * @param source The source buffer.
  * @param destination_len The length of the destination buffer.
  */
 void emb_strncpy(char *destination, const char *source, size_t destination_len) {
-    if (destination == NULL || source == NULL) {
+    if (destination == NULL || source == NULL || destination_len == 0) {
         return;
     }
-    int i = 0;
-    while (i < destination_len && source[i] != '\0') {
-        char current = source[i];
-        destination[i] = current;
-        if (current == '\0') {
-            break;
-        }
-        i++;
+
+    size_t i;
+    for (i = 0; i < destination_len - 1 && source[i] != '\0'; i++) {
+        destination[i] = source[i];
     }
+
+    destination[i] = '\0'; // Null-terminate the destination buffer.
 }
 
 void emb_convert_to_hex_addr(uint64_t addr, char *buffer) {

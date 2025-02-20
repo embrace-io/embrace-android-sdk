cd4d0b9f3f64943647a737d93fbe1de7cc78c6df
Francisco Prieto <priettt@gmail.com>
2025-02-20 12:55:27 -0300

    Remove symbolsDir and projectType from EmbraceExtensionInternal
    Pass them directly to NdkTaskRegistration as Providers

--- embrace-gradle-plugin/src/main/java/io/embrace/android/gradle/plugin/extension/EmbraceExtensionInternal.kt
+++ embrace-gradle-plugin/src/main/java/io/embrace/android/gradle/plugin/extension/EmbraceExtensionInternal.kt
@@ -1,7 +1,5 @@
 package io.embrace.android.gradle.plugin.extension
 
-import io.embrace.android.gradle.plugin.config.ProjectType
-import io.embrace.android.gradle.plugin.config.UnitySymbolsDir
 import io.embrace.android.gradle.plugin.instrumentation.config.model.VariantConfig
 import org.gradle.api.Named
 import org.gradle.api.NamedDomainObjectContainer
@@ -33,14 +31,9 @@ abstract class EmbraceExtensionInternal(
         // convention(EmbraceExtensionInternal.getAppId())
         val config: Property<VariantConfig> = objectFactory.property(VariantConfig::class.java)
 
-        val projectType: Property<ProjectType> = objectFactory.property(ProjectType::class.java)
-        val unitySymbolsDir: Property<UnitySymbolsDir?> = objectFactory.property(UnitySymbolsDir::class.java)
-
         // we need this because older gradle can not inject ObjectFactory
         fun initialize() {
             config.finalizeValueOnRead()
-            projectType.finalizeValueOnRead()
-            unitySymbolsDir.finalizeValueOnRead()
         }
     }
 }
--- embrace-gradle-plugin/src/main/java/io/embrace/android/gradle/plugin/extension/utils/VariantConfigurationToEmbraceExtensionInternal.kt
+++ embrace-gradle-plugin/src/main/java/io/embrace/android/gradle/plugin/extension/utils/VariantConfigurationToEmbraceExtensionInternal.kt
@@ -1,14 +1,8 @@
 package io.embrace.android.gradle.plugin.extension.utils
 
-import io.embrace.android.gradle.plugin.agp.AgpWrapper
-import io.embrace.android.gradle.plugin.config.PluginBehavior
-import io.embrace.android.gradle.plugin.config.ProjectTypeVerifier
-import io.embrace.android.gradle.plugin.config.UnitySymbolsDir
 import io.embrace.android.gradle.plugin.extension.EmbraceExtensionInternal
 import io.embrace.android.gradle.plugin.instrumentation.config.model.VariantConfig
 import io.embrace.android.gradle.plugin.model.AndroidCompactedVariantData
-import io.embrace.android.gradle.plugin.tasks.il2cpp.UnitySymbolFilesManager
-import org.gradle.api.Project
 import org.gradle.api.provider.Provider
 
 /**
@@ -17,47 +11,12 @@ import org.gradle.api.provider.Provider
 class VariantConfigurationToEmbraceExtensionInternal(
     private val variantInfo: AndroidCompactedVariantData,
     private val variantConfigProvider: Provider<VariantConfig>,
-    private val agpWrapper: AgpWrapper,
-    private val behavior: PluginBehavior,
-    private val project: Project
 ) : BaseVariantToEmbraceExtensionInternal(variantInfo.name) {
 
     override fun setupVariant(extension: EmbraceExtensionInternal) {
         extension.variants.named(variantInfo.name).configure {
             // properties from variant configuration
             it.config.set(variantConfigProvider)
-
-            val symbolsDir = getSymbolsDir()
-            val projectType = getProjectType(symbolsDir)
-
-            it.unitySymbolsDir.set(symbolsDir)
-            it.projectType.set(projectType)
         }
-    }
-
-    // there is no need to let Gradle know about the knowledge of how to get unitySymbolsDir, because
-    // all properties that getSymbolsDir depends on are already config-cache aware. Meaning that if any
-    // of the properties that this function uses changes, Gradle will invalidate config cache.
-    private fun getSymbolsDir(): Provider<UnitySymbolsDir> = project.provider {
-        val unityConfig = if (variantConfigProvider.isPresent) {
-            variantConfigProvider.get().embraceConfig?.unityConfig
-        } else {
-            null
-        }
-
-        val realProject = project.parent ?: project
-        UnitySymbolFilesManager.of().getSymbolsDir(
-            realProject.layout.projectDirectory,
-            project.layout.projectDirectory,
-            unityConfig
-        )
-    }
-
-    private fun getProjectType(symbolsDir: Provider<UnitySymbolsDir>) = project.provider {
-        ProjectTypeVerifier.getProjectType(
-            symbolsDir,
-            agpWrapper,
-            behavior,
-        )
     }
 }

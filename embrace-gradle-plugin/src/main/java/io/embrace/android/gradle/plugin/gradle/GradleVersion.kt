3f1c38fc2228d9beb1a28e8121a3c43f0f466587
Francisco Prieto <priettt@gmail.com>
2025-02-21 17:22:24 -0300

    Throw an exception when building with a Gradle version lower than the minimum supported

--- embrace-gradle-plugin/src/main/java/io/embrace/android/gradle/plugin/gradle/GradleVersion.kt
+++ embrace-gradle-plugin/src/main/java/io/embrace/android/gradle/plugin/gradle/GradleVersion.kt
@@ -5,6 +5,7 @@ import org.gradle.util.GradleVersion.version
 sealed class GradleVersion(private val version: org.gradle.util.GradleVersion) :
     Comparable<GradleVersion> {
 
+    object MIN_VERSION : GradleVersion(version("7.5.1"))
     object CURRENT : GradleVersion(org.gradle.util.GradleVersion.current())
     object GRADLE_7_6 : GradleVersion(version("7.6"))
     object GRADLE_8_0 : GradleVersion(version("8.0"))

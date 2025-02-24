a8755f50789ff4ec8976eede45b0d3cf02878abc
Francisco Prieto <priettt@gmail.com>
2025-02-24 11:54:49 -0300

    Fix mapping file JSON bug. (#1925)
    Add a test to verify that the output file is a valid gzipped json file.

--- embrace-gradle-plugin-integration-tests/src/test/java/io/embrace/android/gradle/integration/testcases/RnSourcemapTaskIntegrationTest.kt
+++ embrace-gradle-plugin-integration-tests/src/test/java/io/embrace/android/gradle/integration/testcases/RnSourcemapTaskIntegrationTest.kt
@@ -1,5 +1,6 @@
 package io.embrace.android.gradle.integration.testcases
 
+import com.squareup.moshi.JsonClass
 import io.embrace.android.gradle.integration.framework.IntegrationTestDefaults
 import io.embrace.android.gradle.integration.framework.PluginIntegrationTestRule
 import io.embrace.android.gradle.integration.framework.buildFile
@@ -10,10 +11,14 @@ import io.embrace.android.gradle.network.validateBodyAppId
 import io.embrace.android.gradle.network.validateBodyBuildId
 import io.embrace.android.gradle.network.validateMappingFile
 import io.embrace.android.gradle.plugin.network.EmbraceEndpoint
+import io.embrace.android.gradle.plugin.util.serialization.MoshiSerializer
+import okio.buffer
+import okio.gzip
+import okio.source
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
+import java.io.File
 
 class RnSourcemapTaskIntegrationTest {
 
@@ -27,14 +32,12 @@ class RnSourcemapTaskIntegrationTest {
             fixture = "rn-upload-simple",
             assertions = { projectDir ->
                 // 1. assert that RN bundle ID was injected as a string resource
-                val symbols =
-                    projectDir.buildFile("generated-embrace-resources/values/rn_sourcemap.xml")
+                val symbols = projectDir.buildFile("generated-embrace-resources/values/rn_sourcemap.xml")
                 val expectedResXml = projectDir.file("expected/rn_sourcemap.xml").readText()
                 assertEquals(expectedResXml, symbols.readText())
 
                 // 2. assert that the task output was the bundle + sourcemap file zipped as JSON
-                val output = projectDir.buildFile("output")
-                assertTrue(output.exists() && output.length() > 0)
+                verifyOutputFile(projectDir.buildFile("output"))
 
                 // 3. assert that a request took place
                 val request = fetchRequest(EmbraceEndpoint.SOURCE_MAP)
@@ -50,4 +53,17 @@ class RnSourcemapTaskIntegrationTest {
             }
         )
     }
+
+    private fun verifyOutputFile(outputFile: File) {
+        // unzip the file - this will fail if the file is not gzipped
+        outputFile.source().gzip().buffer().use { source ->
+            // deserialize the JSON - this will fail if the JSON is not valid
+            val bundleAndSourceMap = MoshiSerializer().fromJson(source.readUtf8(), BundleAndSourceMap::class.java)
+            assertEquals("Fake bundle", bundleAndSourceMap.bundle)
+            assertEquals("Fake sourcemap", bundleAndSourceMap.sourcemap)
+        }
+    }
 }
+
+@JsonClass(generateAdapter = true)
+data class BundleAndSourceMap(val bundle: String, val sourcemap: String)

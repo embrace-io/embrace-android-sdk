1475d110d4db5c1aff2184cf9225fba6f80a7bb5
Francisco Prieto <priettt@gmail.com>
2025-05-07 09:30:07 -0300

    Replace bundleAndSourceMapFilesProvider with separate providers

--- embrace-gradle-plugin/src/main/java/io/embrace/android/gradle/plugin/tasks/reactnative/EmbraceRnSourcemapGeneratorTaskRegistration.kt
+++ embrace-gradle-plugin/src/main/java/io/embrace/android/gradle/plugin/tasks/reactnative/EmbraceRnSourcemapGeneratorTaskRegistration.kt
@@ -3,22 +3,25 @@ package io.embrace.android.gradle.plugin.tasks.reactnative
 import io.embrace.android.gradle.plugin.Logger
 import io.embrace.android.gradle.plugin.gradle.nullSafeMap
 import io.embrace.android.gradle.plugin.gradle.registerTask
-import io.embrace.android.gradle.plugin.gradle.safeFlatMap
 import io.embrace.android.gradle.plugin.gradle.tryGetTaskProvider
+import io.embrace.android.gradle.plugin.model.AndroidCompactedVariantData
 import io.embrace.android.gradle.plugin.network.EmbraceEndpoint
 import io.embrace.android.gradle.plugin.tasks.common.RequestParams
 import io.embrace.android.gradle.plugin.tasks.registration.EmbraceTaskRegistration
 import io.embrace.android.gradle.plugin.tasks.registration.RegistrationParams
+import org.gradle.api.Project
 import org.gradle.api.Task
-import org.gradle.api.plugins.ExtraPropertiesExtension.UnknownPropertyException
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.TaskProvider
+import java.io.File
 
 private const val SOURCEMAP_GENERATOR_NAME = "embraceRNSourcemapGeneratorFor"
 private const val REACT_PROPERTY = "react"
 private const val RN_BUNDLE_FILE_EXTENSION = ".bundle"
 private const val RN_SOURCEMAP_FILE_EXTENSION = "bundle.map"
-private const val RN_BUNDLE_KEY = ".bundle"
-private const val RN_SOURCEMAP_KEY = "bundle.map"
+private const val BUNDLE_ASSET_NAME = "bundleAssetName"
+private const val DEFAULT_BUNDLE_NAME = "index.android.bundle"
+private const val SOURCE_MAP_NAME = "android-embrace.bundle.map"
 
 private const val GENERATED_RESOURCE_PATH = "generated/embrace/res"
 
@@ -106,62 +109,91 @@ class EmbraceRnSourcemapGeneratorTaskRegistration : EmbraceTaskRegistration {
                 )
             )
 
-            val bundleAndSourceMapFilesProvider = generatorTask.safeFlatMap { task ->
-                return@safeFlatMap task.outputs.files.asFileTree.elements.nullSafeMap { files ->
-                    return@nullSafeMap files.filter { possibleSoFile ->
-                        possibleSoFile.asFile.absolutePath.endsWith(
-                            RN_BUNDLE_FILE_EXTENSION
-                        ) ||
-                            possibleSoFile.asFile.absolutePath.endsWith(
-                                RN_SOURCEMAP_FILE_EXTENSION
-                            )
-                    }.associate { fileSystemLocation ->
-                        val file = fileSystemLocation.asFile
-                        val key = when {
-                            file.absolutePath.endsWith(RN_BUNDLE_FILE_EXTENSION) -> RN_BUNDLE_KEY
-                            file.absolutePath.endsWith(RN_SOURCEMAP_FILE_EXTENSION) -> RN_SOURCEMAP_KEY
-                            else -> "none"
-                        }
-                        key to project.layout.file(project.provider { file }).get()
-                    }
-                }
-            }
-
-            rnTask.bundleFile.set(
-                bundleAndSourceMapFilesProvider.nullSafeMap {
-                    it[RN_BUNDLE_KEY]
-                }
-            )
-
-            rnTask.sourcemap.set(
-                bundleAndSourceMapFilesProvider.nullSafeMap {
-                    it[RN_SOURCEMAP_KEY]
-                }
-            )
-
-            val reactProperties = try {
-                @Suppress("UNCHECKED_CAST")
-                project.extensions.extraProperties.get(REACT_PROPERTY) as? Map<String, Any>
-            } catch (e: UnknownPropertyException) {
-                null
-            }
-            rnTask.reactProperties.set(reactProperties)
+            rnTask.bundleFile.set(project.layout.file(getBundleFileProvider(generatorTask, project)))
+            rnTask.sourcemap.set(project.layout.file(getSourcemapFileProvider(generatorTask, project, data)))
 
             val flavorName = data.flavorName
             val buildTypeName = data.buildTypeName
-            val bundleFileFolder =
-                if (flavorName.isBlank()) buildTypeName else "$flavorName/$buildTypeName"
+            val bundleFileFolder = if (flavorName.isBlank()) buildTypeName else "$flavorName/$buildTypeName"
             rnTask.sourcemapAndBundleFile.set(
                 project.layout.buildDirectory.file(
                     "outputs/embrace/$bundleFileFolder/$FILE_NAME_SOURCE_MAP_JSON"
                 )
             )
+
+            rnTask.dependsOn(generatorTask)
         } catch (e: Exception) {
-            logger.error(
-                "EmbraceRNSourcemapGeneratorTask failed while getting the Bundle and the SourceMap",
-                e
-            )
+            logger.error("EmbraceRNSourcemapGeneratorTask failed while getting the Bundle and the SourceMap", e)
         }
+    }
+
+    private fun getBundleFileProvider(generatorTask: TaskProvider<Task>, project: Project): Provider<File?> =
+        generatorTask.flatMap { task ->
+            task.outputs.files.asFileTree.elements.flatMap { fileLocations ->
+                val bundleFile = fileLocations.firstOrNull { location ->
+                    location.asFile.name.endsWith(RN_BUNDLE_FILE_EXTENSION)
+                }?.asFile
+
+                if (bundleFile != null && bundleFile.exists()) {
+                    project.provider { bundleFile }
+                } else {
+                    findBundleFile(project)
+                }
+            }
+        }
+
+    /**
+     * Iterate through all folders inside build folder to find index.android.bundle
+     */
+    private fun findBundleFile(project: Project): Provider<File?> {
+        return project.layout.buildDirectory.nullSafeMap { buildDir ->
+            buildDir.asFile.walk()
+                .filter { it.isFile && (it.name == "index.android.bundle") }
+                .firstOrNull()
+        }
+    }
+
+    private fun getSourcemapFileProvider(
+        generatorTask: TaskProvider<Task>,
+        project: Project,
+        data: AndroidCompactedVariantData,
+    ): Provider<File?> =
+        generatorTask.flatMap { task ->
+            task.outputs.files.asFileTree.elements.flatMap { fileLocations ->
+                val sourcemapFile = fileLocations.firstOrNull { location ->
+                    location.asFile.name.endsWith(RN_SOURCEMAP_FILE_EXTENSION)
+                }?.asFile
+
+                if (sourcemapFile != null && sourcemapFile.exists()) {
+                    project.provider { sourcemapFile }
+                } else {
+                    findSourcemapFile(project, data)
+                }
+            }
+        }
+
+    private fun findSourcemapFile(project: Project, data: AndroidCompactedVariantData): Provider<File?> {
+        return project.layout.buildDirectory.nullSafeMap { buildDir ->
+            File(buildDir.asFile, "generated/sourcemaps/$SOURCE_MAP_NAME").takeIf { it.exists() }
+                ?: File(buildDir.asFile, getReactNativeSourcemapFilePath(project, data.name)).takeIf { it.exists() }
+        }
+    }
+
+    /**
+     * Look for sourcemap file in generated/sourcemaps/react/variantDir
+     * If a custom sourcemap file name has been set in a gradle property, use it.
+     * If not, use the default name.
+     */
+    private fun getReactNativeSourcemapFilePath(project: Project, variantName: String): String {
+        val sourceMapsDirPath = "generated/sourcemaps/react/$variantName"
+        val reactProperties = try {
+            project.extensions.extraProperties.get(REACT_PROPERTY) as? Map<*, *>
+        } catch (e: Exception) {
+            null
+        }
+        val bundleAssetName = reactProperties?.get(BUNDLE_ASSET_NAME)?.toString() ?: DEFAULT_BUNDLE_NAME
+
+        return "$sourceMapsDirPath/$bundleAssetName.map"
     }
 
     private companion object {

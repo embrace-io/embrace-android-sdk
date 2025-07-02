5fc4a2641be68872a0a058593a0811cca45156de
Francisco Prieto <priettt@gmail.com>
2025-07-02 16:06:53 -0300

    Migrate to vanniktech maven publish (#2300)

--- embrace-gradle-plugin/build.gradle.kts
+++ embrace-gradle-plugin/build.gradle.kts
@@ -2,8 +2,7 @@ plugins {
     kotlin("jvm")
     id("java-gradle-plugin")
     id("groovy")
-    id("maven-publish")
-    id("signing")
+    id("com.vanniktech.maven.publish")
     alias(libs.plugins.google.ksp)
     alias(libs.plugins.buildconfig)
     id("io.embrace.internal.build-logic")
@@ -37,17 +36,6 @@ buildConfig {
     buildConfigField("String", "VERSION", "\"$version\"")
 }
 
-allprojects {
-    extra["signing.keyId"] = System.getenv("mavenSigningKeyId")
-    extra["signing.secretKeyRingFile"] = System.getenv("mavenSigningKeyRingFile")
-    extra["signing.password"] = System.getenv("mavenSigningKeyPassword")
-}
-
-java {
-    withJavadocJar()
-    withSourcesJar()
-}
-
 // marker artifact publication
 gradlePlugin {
     plugins {
@@ -61,105 +49,35 @@ gradlePlugin {
     }
 }
 
-// maven-publish plugin publications settings
-publishing {
-    publications {
-        create<MavenPublication>("pluginMaven") {
-            pom {
-                artifactId = "embrace-swazzler"
-                name = "embrace-swazzler"
-                group = "io.embrace"
-                description = "Embrace Gradle Plugin"
-                url = "https://github.com/embrace-io/embrace-android-sdk"
-                licenses {
-                    license {
-                        name = "Embrace License"
-                        url = "https://embrace.io/docs/terms-of-service/"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "dev1"
-                        name = "Embrace"
-                        email = "support@embrace.io"
-                    }
-                }
-                scm {
-                    connection = "scm:git:github.com/embrace-io/embrace-android-sdk.git"
-                    developerConnection = "scm:git:ssh://github.com/embrace-io/embrace-android-sdk.git"
-                    url = "https://github.com/embrace-io/embrace-android-sdk/tree/main"
-                }
-            }
-        }
-    }
+// Configure Vanniktech Maven Publish Plugin
+mavenPublishing {
+    coordinates("io.embrace", "embrace-swazzler", project.version.toString())
 
-    afterEvaluate {
-        publications {
-            named<MavenPublication>("embraceGradlePluginMarkerMaven") {
-                pom {
-                    name = "embrace-swazzler"
-                    artifactId = "io.embrace.swazzler.gradle.plugin"
-                    group = "io.embrace"
-                    description = "Embrace Gradle Plugin"
-                    url = "https://github.com/embrace-io/embrace-android-sdk"
-                    licenses {
-                        license {
-                            name = "Embrace License"
-                            url = "https://embrace.io/docs/terms-of-service/"
-                        }
-                    }
-                    developers {
-                        developer {
-                            id = "dev1"
-                            name = "Embrace"
-                            email = "support@embrace.io"
-                        }
-                    }
-                    scm {
-                        connection = "scm:git:github.com/embrace-io/embrace-android-sdk.git"
-                        developerConnection = "scm:git:ssh://github.com/embrace-io/embrace-android-sdk.git"
-                        url = "https://github.com/embrace-io/embrace-android-sdk/tree/main"
-                    }
-                }
-            }
-        }
-        signing {
-            setRequired { gradle.taskGraph.hasTask("publishEmbraceGradlePluginMarkerMavenPublicationToSonatypeRepository") }
-            sign(publishing.publications["embraceGradlePluginMarkerMaven"])
-        }
-    }
+    publishToMavenCentral()
+    signAllPublications()
 
-    repositories {
-        // beta releases
-        maven {
-            credentials {
-                username = System.getenv("MAVEN_QA_USER")
-                password = System.getenv("MAVEN_QA_PASSWORD")
+    pom {
+        name = "embrace-swazzler"
+        group = "io.embrace"
+        description = "Embrace Gradle Plugin"
+        url = "https://github.com/embrace-io/embrace-android-sdk"
+        licenses {
+            license {
+                name = "Embrace License"
+                url = "https://embrace.io/docs/terms-of-service/"
             }
-            name = "Qa"
-            url = uri("https://repo.embrace.io/repository/beta")
         }
-        // the android-testing maven repository is used for publishing snapshots
-        maven {
-            credentials {
-                username = System.getenv("MAVEN_QA_USER")
-                password = System.getenv("MAVEN_QA_PASSWORD")
+        developers {
+            developer {
+                id = "dev1"
+                name = "Embrace"
+                email = "support@embrace.io"
             }
-            name = "Snapshot"
-            url = uri("https://repo.embrace.io/repository/android-testing")
         }
-        // sonatype repo that provides path to publishing on mavencentral
-        maven {
-            credentials {
-                username = System.getenv("SONATYPE_USERNAME")
-                password = System.getenv("SONATYPE_PASSWORD")
-            }
-            name = "sonatype"
-            url = uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
+        scm {
+            connection = "scm:git:github.com/embrace-io/embrace-android-sdk.git"
+            developerConnection = "scm:git:ssh://github.com/embrace-io/embrace-android-sdk.git"
+            url = "https://github.com/embrace-io/embrace-android-sdk/tree/main"
         }
-    }
-    signing {
-        setRequired { gradle.taskGraph.hasTask("publishPluginMavenPublicationToSonatypeRepository") }
-        sign(publishing.publications["pluginMaven"])
     }
 }

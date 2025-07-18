073569f1f094f53c3a089ed264118199e8a0461f
bidetofevil <hanson.ho@embrace.io>
2025-07-18 15:41:07 -0700

    WIP Rebase limts change on Kotlin API changes (need version bump)

--- embrace-android-sdk/src/integrationTest/kotlin/io/embrace/android/embracesdk/testcases/features/LogFeatureTest.kt
+++ embrace-android-sdk/src/integrationTest/kotlin/io/embrace/android/embracesdk/testcases/features/LogFeatureTest.kt
@@ -18,12 +18,13 @@ import io.embrace.android.embracesdk.testframework.SdkIntegrationTestRule
 import io.embrace.opentelemetry.kotlin.ExperimentalApi
 import io.embrace.opentelemetry.kotlin.aliases.OtelJavaSeverity
 import io.embrace.opentelemetry.kotlin.logging.model.SeverityNumber
-import java.util.LinkedList
-import java.util.Queue
+import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.LinkedList
+import java.util.Queue
 
 @OptIn(ExperimentalApi::class)
 @RunWith(AndroidJUnit4::class)

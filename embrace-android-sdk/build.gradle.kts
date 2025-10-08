e363f6c4a4dbda39e399e2349b14aaed1fd31009
priettt <priettt@gmail.com>
2025-10-08 11:55:28 -0300

    Bump NDK to 29 (latest stable version)

--- embrace-android-sdk/build.gradle.kts
+++ embrace-android-sdk/build.gradle.kts
@@ -12,7 +12,7 @@ embrace {
 description = "Embrace Android SDK: Core"
 
 android {
-    ndkVersion = "27.3.13750724"
+    ndkVersion = "29.0.14206865"
 
     defaultConfig {
         namespace = "io.embrace.android.embracesdk"

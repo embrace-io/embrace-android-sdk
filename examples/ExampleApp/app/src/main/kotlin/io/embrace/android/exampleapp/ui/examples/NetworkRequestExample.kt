60001d09a62911a5962e28869ac94692a98b15b2
Hanson Ho <hanson.ho@embrace.io>
2025-10-21 07:34:51 -0700

    Add HUC network requests to example app (#2603)
    ## Goal
    
    Add manual way to run HUC network requests. Also bumped Embrace version to the latest.

--- examples/ExampleApp/app/src/main/kotlin/io/embrace/android/exampleapp/ui/examples/NetworkRequestExample.kt
+++ examples/ExampleApp/app/src/main/kotlin/io/embrace/android/exampleapp/ui/examples/NetworkRequestExample.kt
@@ -8,11 +8,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import io.embrace.android.embracesdk.Embrace
 import io.embrace.android.exampleapp.ui.RadioButtonList
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
+import java.net.URL
+import java.nio.charset.StandardCharsets
+import java.util.concurrent.Executors
+import javax.net.ssl.HttpsURLConnection
 
 private enum class RequestType {
     GET_REQUEST,
@@ -22,6 +27,7 @@ private enum class RequestType {
 }
 
 private val client = OkHttpClient.Builder().build()
+private val hucExecutor = Executors.newFixedThreadPool(2)
 
 @Composable
 fun NetworkRequestExample() {
@@ -42,24 +48,60 @@ fun NetworkRequestExample() {
                 }
 
                 override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
-                    Log.i("EmbraceTestApp", "Network request completed ${response.code} ${response.body?.string()}")
+                    Log.i("EmbraceTestApp", "Network request completed ${response.code} ${response.body.string()}")
                 }
             })
         },
     ) {
-        Text("Make network request")
+        Text("Make OkHttp network request")
+    }
+    Button(
+        onClick = {
+            val url = getUrl(requestValue)
+            hucExecutor.submit {
+                var connection: HttpsURLConnection? = null
+                try {
+                    connection = URL(url).openConnection() as HttpsURLConnection
+                    connection.setRequestProperty("Content-Type", String.format("application/json;charset=%s", StandardCharsets.UTF_8))
+                    connection.connectTimeout = 5000
+                    connection.readTimeout = 15000
+
+                    if (requestValue == RequestType.POST_REQUEST) {
+                        connection.requestMethod = "POST"
+                        connection.doInput = true
+                        connection.doOutput = true
+                        connection.outputStream.use { os ->
+                            os.write("body".toByteArray(StandardCharsets.UTF_8))
+                        }
+                    }
+
+                    val responseCode = connection.responseCode
+                    if (responseCode == 200) {
+                        Embrace.getInstance().addBreadcrumb("Successful request to $url")
+                    } else {
+                        Embrace.getInstance().addBreadcrumb("Non-successful request to $url with response code $responseCode")
+                    }
+                } catch (t: Throwable) {
+                    Embrace.getInstance().addBreadcrumb("Error while making HUC network request: $t")
+                } finally {
+                    connection?.disconnect()
+                }
+            }
+        },
+    ) {
+        Text("Make HUC network request")
     }
 }
 
-private fun prepareRequest(requestType: RequestType): Request {
-    val builder = Request.Builder()
+private fun getUrl(requestValue: RequestType): String = when (requestValue) {
+    RequestType.GET_REQUEST -> "https://httpbin.org/get"
+    RequestType.POST_REQUEST -> "https://httpbin.org/post"
+    RequestType.NOT_FOUND_REQUEST -> "https://httpbin.org/status/404"
+    RequestType.INVALID_REQUEST -> "https://some-invalid-url.path"
+}
 
-    when (requestType) {
-        RequestType.GET_REQUEST -> builder.url("https://httpbin.org/get")
-        RequestType.POST_REQUEST -> builder.url("https://httpbin.org/post")
-        RequestType.NOT_FOUND_REQUEST -> builder.url("https://httpbin.org/status/404")
-        RequestType.INVALID_REQUEST -> builder.url("https://some-invalid-url.path")
-    }
+private fun prepareRequest(requestType: RequestType): Request {
+    val builder = Request.Builder().url(getUrl(requestType))
     if (requestType == RequestType.POST_REQUEST) {
         builder.post("{}".toRequestBody("application/json".toMediaType()))
     }

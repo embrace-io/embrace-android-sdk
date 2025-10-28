d8b550692724a5b307f22be45234d28da1cd326d
Jamie Lynch <fractalwrench@gmail.com>
2025-10-28 14:47:45 +0000

    poc: network status instrumentation modularisation

--- embrace-android-features/src/main/kotlin/io/embrace/android/embracesdk/internal/injection/FeatureModule.kt
+++ embrace-android-features/src/main/kotlin/io/embrace/android/embracesdk/internal/injection/FeatureModule.kt
@@ -1,7 +1,6 @@
 package io.embrace.android.embracesdk.internal.injection
 
 import io.embrace.android.embracesdk.internal.arch.datasource.DataSourceState
-import io.embrace.android.embracesdk.internal.capture.connectivity.NetworkStatusDataSource
 import io.embrace.android.embracesdk.internal.capture.crumbs.BreadcrumbDataSource
 import io.embrace.android.embracesdk.internal.capture.crumbs.RnActionDataSource
 import io.embrace.android.embracesdk.internal.capture.telemetry.InternalErrorDataSource
@@ -10,5 +9,4 @@ interface FeatureModule {
     val breadcrumbDataSource: DataSourceState<BreadcrumbDataSource>
     val rnActionDataSource: DataSourceState<RnActionDataSource>
     val internalErrorDataSource: DataSourceState<InternalErrorDataSource>
-    val networkStatusDataSource: DataSourceState<NetworkStatusDataSource>
 }

6a42e2937169ccee9636ea58d71935388f1a2708
Francisco Prieto <priettt@gmail.com>
2025-11-10 13:37:15 -0300

    Rename swazzler artifact to embrace-gradle-plugin

--- embrace-gradle-plugin-integration-tests/fixtures/android-3rd-party-symbols/settings.gradle.kts
+++ embrace-gradle-plugin-integration-tests/fixtures/android-3rd-party-symbols/settings.gradle.kts
@@ -11,7 +11,7 @@ pluginManagement {
         val snapshotVersion = extra["plugin_snapshot_version"] as String
 
         id("com.android.application").version(agpVersion)
-        id("io.embrace.swazzler").version(snapshotVersion)
+        id("io.embrace.gradle").version(snapshotVersion)
         id("io.embrace.android.testplugin").version(snapshotVersion)
     }
 }

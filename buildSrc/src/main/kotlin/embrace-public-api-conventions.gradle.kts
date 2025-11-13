dd6ad6897fcb4b3c59dbd0394f8a688cad41368b
Jamie Lynch <fractalwrench@gmail.com>
2025-11-13 10:47:38 +0000

    chore: bump dokka to v2

--- buildSrc/src/main/kotlin/embrace-public-api-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/embrace-public-api-conventions.gradle.kts
@@ -7,6 +7,31 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 plugins {
     id("embrace-prod-android-conventions")
     id("binary-compatibility-validator")
+    id("org.jetbrains.dokka")
 }
 
 kotlin.explicitApi()
+
+dokka {
+    dokkaPublications.html {
+        failOnWarning.set(true)
+    }
+    dokkaSourceSets.main {
+        perPackageOption {
+            matchingRegex.set(".*internal.*")
+            suppress.set(true)
+            reportUndocumented.set(true) // Emit warnings about not documented members
+        }
+        sourceLink {
+            localDirectory.set(file("src/main/kotlin"))
+            remoteUrl("https://github.com/embrace-io/embrace-android-sdk/blob/main/${project.name}/src/main/kotlin")
+            remoteLineSuffix.set("#L")
+        }
+        externalDocumentationLinks.register("embrace-io-docs") {
+            url("https://embrace.io/docs/android")
+        }
+    }
+    pluginsConfiguration.html {
+        footerMessage.set("(c) embrace.io")
+    }
+}

da2f27ae6ea1e1800b4f01342aee621f5f13a47f
Jamie Lynch <fractalwrench@gmail.com>
2025-11-17 16:36:19 +0000

    refactor: improve dependency injection

--- embrace-android-core/src/test/java/io/embrace/android/embracesdk/internal/injection/DependencyInjectionKtTest.kt
+++ embrace-android-core/src/test/java/io/embrace/android/embracesdk/internal/injection/DependencyInjectionKtTest.kt
@@ -6,29 +6,13 @@ import java.util.concurrent.atomic.AtomicInteger
 
 internal class DependencyInjectionKtTest {
 
-    private val factoryCounter = AtomicInteger(0)
-    private val eagerSingletonCounter = AtomicInteger(0)
     private val lazySingletonCounter = AtomicInteger(0)
-
-    private val factory by factory { factoryCounter.incrementAndGet() }
-    private val eagerSingleton by singleton(LoadType.EAGER) { eagerSingletonCounter.incrementAndGet() }
-    private val lazySingleton by singleton(LoadType.LAZY) { lazySingletonCounter.incrementAndGet() }
+    private val lazySingleton by singleton { lazySingletonCounter.incrementAndGet() }
 
     @Test
     fun testInjection() {
-        // assert defaults
-        assertEquals(0, factoryCounter.get())
-        assertEquals(1, eagerSingletonCounter.get())
         assertEquals(0, lazySingletonCounter.get())
-
-        // get values from properties
-        assertEquals(1, factory)
-        assertEquals(1, eagerSingleton)
         assertEquals(1, lazySingleton)
-
-        // get values again from properties
-        assertEquals(2, factory)
-        assertEquals(1, eagerSingleton)
         assertEquals(1, lazySingleton)
     }
 }

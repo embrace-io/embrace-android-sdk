648e898824b119b97da2336281c5e71b822938f1
Jamie Lynch <fractalwrench@gmail.com>
2025-11-24 14:54:25 +0000

    build: reduce dependencies in features module

--- embrace-android-features/build.gradle.kts
+++ embrace-android-features/build.gradle.kts
@@ -9,36 +9,13 @@ android {
 }
 
 dependencies {
-    compileOnly(project(":embrace-android-api"))
     compileOnly(project(":embrace-android-core"))
-    compileOnly(project(":embrace-android-infra"))
-    compileOnly(project(":embrace-android-utils"))
-    compileOnly(project(":embrace-android-payload"))
-    compileOnly(project(":embrace-internal-api"))
-    compileOnly(project(":embrace-android-otel"))
     implementation(project(":embrace-android-instrumentation-api"))
-    implementation(project(":embrace-android-instrumentation-schema"))
-
-    implementation(libs.lifecycle.process)
     implementation(libs.opentelemetry.kotlin.api)
-    implementation(libs.opentelemetry.kotlin.api.ext)
     implementation(libs.opentelemetry.kotlin.semconv)
 
-    testImplementation(project(":embrace-android-api"))
     testImplementation(project(":embrace-android-core"))
-    testImplementation(project(":embrace-android-infra"))
-    testImplementation(project(":embrace-android-utils"))
-    testImplementation(project(":embrace-android-payload"))
-    testImplementation(project(":embrace-internal-api"))
-    testImplementation(project(":embrace-android-otel"))
     testImplementation(project(":embrace-test-fakes"))
-    testImplementation(project(":embrace-test-common"))
-    testImplementation(project(":embrace-android-config-fakes"))
-    testImplementation(project(":embrace-android-otel-fakes"))
     testImplementation(project(":embrace-android-instrumentation-api-fakes"))
     testImplementation(libs.robolectric)
-    testImplementation(platform(libs.opentelemetry.bom))
-    testImplementation(libs.opentelemetry.api)
-    testImplementation(libs.opentelemetry.context)
-    testImplementation(libs.mockk)
 }

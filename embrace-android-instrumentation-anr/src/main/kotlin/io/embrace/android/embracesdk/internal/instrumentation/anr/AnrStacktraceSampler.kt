38e2f6130aefe49538c23e86a1e97b513cb2cf71
Jamie Lynch <fractalwrench@gmail.com>
2025-12-02 16:37:47 +0000

    refactor: combine thread info collector

--- embrace-android-instrumentation-anr/src/main/kotlin/io/embrace/android/embracesdk/internal/instrumentation/anr/AnrStacktraceSampler.kt
+++ embrace-android-instrumentation-anr/src/main/kotlin/io/embrace/android/embracesdk/internal/instrumentation/anr/AnrStacktraceSampler.kt
@@ -1,5 +1,6 @@
 package io.embrace.android.embracesdk.internal.instrumentation.anr
 
+import io.embrace.android.embracesdk.internal.arch.stacktrace.getThreadInfo
 import io.embrace.android.embracesdk.internal.clock.Clock
 import io.embrace.android.embracesdk.internal.instrumentation.anr.detection.ThreadBlockageEvent
 import io.embrace.android.embracesdk.internal.instrumentation.anr.detection.ThreadBlockageListener
@@ -7,6 +8,7 @@ import io.embrace.android.embracesdk.internal.instrumentation.anr.detection.Thre
 import io.embrace.android.embracesdk.internal.instrumentation.anr.payload.AnrInterval
 import io.embrace.android.embracesdk.internal.instrumentation.anr.payload.AnrSample
 import io.embrace.android.embracesdk.internal.instrumentation.anr.payload.AnrSampleList
+import io.embrace.android.embracesdk.internal.payload.ThreadInfo
 import io.embrace.android.embracesdk.internal.session.MemoryCleanerListener
 import io.embrace.android.embracesdk.internal.worker.BackgroundWorker
 import java.util.concurrent.CopyOnWriteArrayList
@@ -16,17 +18,17 @@ import java.util.concurrent.CopyOnWriteArrayList
  */
 internal class AnrStacktraceSampler(
     private val clock: Clock,
-    targetThread: Thread,
+    private val targetThread: Thread,
     private val anrMonitorWorker: BackgroundWorker,
-    val maxIntervalsPerSession: Int,
-    val maxStacktracesPerInterval: Int,
-    val stacktraceFrameLimit: Int,
+    private val maxIntervalsPerSession: Int,
+    private val maxStacktracesPerInterval: Int,
+    private val stacktraceFrameLimit: Int,
 ) : ThreadBlockageListener, MemoryCleanerListener {
 
     val anrIntervals: CopyOnWriteArrayList<AnrInterval> = CopyOnWriteArrayList<AnrInterval>()
     private val samples = mutableListOf<AnrSample>()
+    private val currentStacktraceStates: MutableMap<Long, ThreadInfo> = HashMap()
     private var lastUnblockedMs: Long = 0
-    private val threadInfoCollector = ThreadInfoCollector(targetThread, stacktraceFrameLimit)
 
     fun size(): Int = samples.size
 
@@ -42,7 +44,7 @@ internal class AnrStacktraceSampler(
     }
 
     private fun onThreadBlocked(timestamp: Long) {
-        threadInfoCollector.clearStacktraceCache()
+        clearStacktraceCache()
         lastUnblockedMs = timestamp
     }
 
@@ -52,7 +54,7 @@ internal class AnrStacktraceSampler(
             AnrSample(timestamp, null, 0, AnrSample.CODE_SAMPLE_LIMIT_REACHED)
         } else {
             val start = clock.now()
-            val threads = threadInfoCollector.captureSample()
+            val threads = captureSample()
             val sampleOverheadMs = clock.now() - start
             AnrSample(timestamp, threads, sampleOverheadMs)
         }
@@ -88,7 +90,7 @@ internal class AnrStacktraceSampler(
         // reset state
         samples.clear()
         lastUnblockedMs = timestamp
-        threadInfoCollector.clearStacktraceCache()
+        clearStacktraceCache()
     }
 
     /**
@@ -96,7 +98,6 @@ internal class AnrStacktraceSampler(
      * intervals with samples has been reached & the SDK needs to discard samples. We attempt
      * to pick the least valuable interval in this case.
      */
-
     fun findLeastValuableIntervalWithSamples(): AnrInterval? =
         findIntervalsWithSamples().minByOrNull(AnrInterval::duration)
 
@@ -139,6 +140,42 @@ internal class AnrStacktraceSampler(
             return results.map(AnrInterval::deepCopy)
         }
     }
+
+    /**
+     * Clears the stacktrace cache for all threads.
+     */
+    private fun clearStacktraceCache(): Unit = currentStacktraceStates.clear()
+
+    /**
+     * Captures the thread traces required for the given sample.
+     */
+    private fun captureSample(): List<ThreadInfo> {
+        val threadInfo = getMainThread()
+        val sanitizedThreads = mutableListOf<ThreadInfo>()
+
+        // Compares main thread with the last known thread state via hashcode. If hashcode changed
+        // it should be added to the anrInfo list and also the currentAnrInfoState must be updated.
+        val threadId = threadInfo.threadId
+        val cache: ThreadInfo? = currentStacktraceStates[threadId]
+
+        // only serialize if the previous stacktrace doesn't match.
+        if (cache == null || threadInfo != cache) {
+            sanitizedThreads.add(threadInfo)
+            currentStacktraceStates[threadId] = threadInfo
+        }
+        return sanitizedThreads
+    }
+
+    /**
+     * Filter the thread list based on allow/block list get by config.
+     *
+     * @return filtered threads
+     */
+    private fun getMainThread(): ThreadInfo = getThreadInfo(
+        targetThread,
+        targetThread.stackTrace,
+        stacktraceFrameLimit
+    )
 
     private companion object {
 

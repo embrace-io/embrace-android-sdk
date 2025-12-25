29ecab993b77a0aedf983e55424f502b28503827
bidetofevil <hanson.ho@embrace.io>
2025-12-24 22:28:34 -0800

    Create OTel-agnostic service to create OTel logs

---
+++ embrace-android-otel/src/main/kotlin/io/embrace/android/embracesdk/internal/otel/logs/EventServiceImpl.kt
@@ -0,0 +1,66 @@
+package io.embrace.android.embracesdk.internal.otel.logs
+
+import io.embrace.android.embracesdk.Severity
+import io.embrace.android.embracesdk.internal.arch.schema.PrivateSpan
+import io.embrace.android.embracesdk.internal.arch.schema.SchemaType
+import io.embrace.android.embracesdk.internal.utils.Provider
+import io.embrace.android.embracesdk.internal.utils.Uuid
+import io.embrace.opentelemetry.kotlin.ExperimentalApi
+import io.embrace.opentelemetry.kotlin.logging.Logger
+import io.embrace.opentelemetry.kotlin.logging.model.SeverityNumber
+import io.embrace.opentelemetry.kotlin.semconv.IncubatingApi
+import io.embrace.opentelemetry.kotlin.semconv.LogAttributes
+import java.util.concurrent.TimeUnit
+
+@OptIn(ExperimentalApi::class, IncubatingApi::class)
+class EventServiceImpl(
+    loggerProvider: Provider<Logger>
+) : EventService {
+
+    private val logger: Logger = loggerProvider()
+
+    override fun log(
+        logTimeMs: Long,
+        schemaType: SchemaType,
+        severity: Severity,
+        message: String,
+        isPrivate: Boolean,
+        embraceAttributes: Map<String, String>
+    ) {
+        val severityNumber = when (severity) {
+            Severity.INFO -> SeverityNumber.INFO
+            Severity.WARNING -> SeverityNumber.WARN
+            Severity.ERROR -> SeverityNumber.ERROR
+        }
+        logger.log(
+            body = message,
+            severityNumber = severityNumber,
+            severityText = getSeverityText(severityNumber),
+            timestamp = TimeUnit.MILLISECONDS.toNanos(logTimeMs)
+        ) {
+            if (!embraceAttributes.contains(LogAttributes.LOG_RECORD_UID)) {
+                setStringAttribute(LogAttributes.LOG_RECORD_UID, Uuid.getEmbUuid())
+            }
+
+            if (isPrivate) {
+                setStringAttribute(PrivateSpan.key.name, PrivateSpan.value)
+            }
+
+            with(schemaType) {
+                setStringAttribute(telemetryType.key.name, telemetryType.value)
+                attributes().forEach {
+                    setStringAttribute(it.key, it.value)
+                }
+            }
+
+            embraceAttributes.forEach {
+                setStringAttribute(it.key, it.value)
+            }
+        }
+    }
+
+    private fun getSeverityText(severity: SeverityNumber) = when (severity) {
+        SeverityNumber.WARN -> "WARNING"
+        else -> severity.name
+    }
+}

96838457b375e74df8d7d0366a4d73441e4e5a67
Francisco Prieto <priettt@gmail.com>
2026-01-28 18:17:54 -0300

    Fix benchmark tests and update ExampleApp to AGP 9

--- embrace-microbenchmark/src/androidTest/kotlin/io/embrace/android/embracesdk/benchmark/TracingApiBenchmarks.kt
+++ embrace-microbenchmark/src/androidTest/kotlin/io/embrace/android/embracesdk/benchmark/TracingApiBenchmarks.kt
@@ -14,7 +14,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class TracingApiBenchmarks: RobolectricTest() {
+class TracingApiBenchmarks {
 
     @get:Rule
     val benchmarkRule = BenchmarkRule()

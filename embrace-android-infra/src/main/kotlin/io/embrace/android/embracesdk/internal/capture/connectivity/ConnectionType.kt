6ed88192708c5326540b94c266ec08daab8623e1
bidetofevil <hanson.ho@embrace.io>
2026-03-18 16:21:41 -0700

    Make NetworkStateDataSource use ConnectivityStatus

--- embrace-android-infra/src/main/kotlin/io/embrace/android/embracesdk/internal/capture/connectivity/ConnectionType.kt
+++ embrace-android-infra/src/main/kotlin/io/embrace/android/embracesdk/internal/capture/connectivity/ConnectionType.kt
@@ -1,13 +1,16 @@
 package io.embrace.android.embracesdk.internal.capture.connectivity
 
+import io.embrace.android.embracesdk.internal.comms.delivery.NetworkStatus
+
 /**
  * Type that an app's network connection is classified into.
  */
 enum class ConnectionType(
     val openForDelivery: Boolean,
+    val typeName: String
 ) {
-    WIFI(openForDelivery = true),
-    WAN(openForDelivery = true),
-    UNKNOWN(openForDelivery = true),
-    NONE(openForDelivery = false),
+    WIFI(openForDelivery = true, typeName = NetworkStatus.WIFI.value),
+    WAN(openForDelivery = true, typeName = NetworkStatus.WAN.value),
+    UNKNOWN(openForDelivery = true, typeName = NetworkStatus.UNKNOWN.value),
+    NONE(openForDelivery = false, typeName = NetworkStatus.NOT_REACHABLE.value),
 }

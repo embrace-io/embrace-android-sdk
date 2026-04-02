1ccdc7e6e3051355316d287b2c95ea58f0dc65e3
Jamie Lynch <fractalwrench@gmail.com>
2026-04-02 15:11:16 +0100

    feat: implement remote config fields for user session

--- embrace-android-config/src/main/kotlin/io/embrace/android/embracesdk/internal/config/behavior/SessionBehavior.kt
+++ embrace-android-config/src/main/kotlin/io/embrace/android/embracesdk/internal/config/behavior/SessionBehavior.kt
@@ -11,4 +11,22 @@ interface SessionBehavior {
      * Returns the maximum number of properties that can be attached to a session
      */
     fun getMaxSessionProperties(): Int
+
+    /**
+     * Returns the maximum allowed duration of a user session in milliseconds.
+     * Defaults to 24 hours.
+     */
+    fun getMaxSessionDurationMs(): Long
+
+    /**
+     * Returns the inactivity timeout for a user session in milliseconds.
+     * Must be <= [getMaxSessionDurationMs]. Defaults to 30 minutes.
+     */
+    fun getSessionInactivityTimeoutMs(): Long
+
+    /**
+     * Returns the minimum duration a session must last before it can be ended manually,
+     * in milliseconds.
+     */
+    fun getMinSessionDurationMs(): Long
 }

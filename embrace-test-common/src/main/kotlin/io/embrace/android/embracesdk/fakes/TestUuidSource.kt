3f3b7db678de4cbf7023f4692d1a193e7a47598b
bidetofevil <hanson.ho@embrace.io>
2026-05-06 09:08:33 -0700

    Remove synchronization for test UuidSource

--- embrace-test-common/src/main/kotlin/io/embrace/android/embracesdk/fakes/TestUuidSource.kt
+++ embrace-test-common/src/main/kotlin/io/embrace/android/embracesdk/fakes/TestUuidSource.kt
@@ -5,11 +5,10 @@ import io.embrace.android.embracesdk.internal.utils.UuidSourceImpl
 import kotlin.random.Random
 
 /**
- * A [UuidSource] used for tests that is thread-safe and generates UUIDs deterministically
+ * A [UuidSource] used for tests that generates UUIDs deterministically
  */
 class TestUuidSource : UuidSource {
     private val delegate: UuidSource = UuidSourceImpl(Random(0))
 
-    @Synchronized
     override fun createUuid(): String = delegate.createUuid()
 }

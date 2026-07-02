9ecb3a9972b56a390eec5dbe6c59ac76f83333c1
Hanson Ho <hanson.ho@embrace.io>
2026-07-02 10:21:54 -0700

    Update logcat span output and setup traceparent allowlist (#3536)
    <!-- Describe how this change has been tested -->
    
    Add explicit allowlist to enable network span forwarding and traceparent injection to happen on certain domains. Added some more customzied span logging output in logcat span exporter

--- examples/ExampleApp/app/src/main/kotlin/io/embrace/android/exampleapp/LogcatSpanExporter.kt
+++ examples/ExampleApp/app/src/main/kotlin/io/embrace/android/exampleapp/LogcatSpanExporter.kt
@@ -4,19 +4,33 @@ import android.util.Log
 import io.opentelemetry.sdk.common.CompletableResultCode
 import io.opentelemetry.sdk.trace.data.SpanData
 import io.opentelemetry.sdk.trace.export.SpanExporter
+import kotlin.collections.filter
 
 class LogcatSpanExporter : SpanExporter {
 
     override fun export(spans: Collection<SpanData>): CompletableResultCode {
         for (span in spans) {
-            Log.i(
-                "EmbraceTestApp",
-                "Span: ${span.name}, TraceId: ${span.traceId}, SpanId: ${span.spanId}, Attributes: ${span.attributes}, Events: ${span.events}, Links: ${span.links},"
-            )
+            Log.i("EmbraceTestApp","[Span] ${span.getSpanSummary()}")
         }
         return CompletableResultCode.ofSuccess()
     }
 
     override fun flush(): CompletableResultCode = CompletableResultCode.ofSuccess()
     override fun shutdown(): CompletableResultCode = CompletableResultCode.ofSuccess()
+
+    private fun SpanData.getSpanSummary(): String {
+        val attrs = attributes.asMap().mapKeys { it.key.key }
+        val type = attrs["emb.type"]
+        val customMessage = when (type) {
+            "ux.session" -> {
+                attrs.filter { it.key.contains("session") }.let { filtered ->
+                    val id = attrs.entries.single { it.key == "emb.session_part_id" }.value
+                    "`Session Part $id` $filtered"
+                }
+            }
+            else -> "`$name`, TraceId: $traceId, SpanId: $spanId, Attributes: $attrs, Events: $events, Links: $links"
+        }
+
+        return "$type: $customMessage"
+    }
 }

5c7e3f20d0d0e25a9fc1cd136e86159d3510930c
Jamie Lynch <fractalwrench@gmail.com>
2026-08-04 10:42:45 +0100

    fix build (#3728)

--- embrace-android-sdk/src/integrationTest/kotlin/io/embrace/android/embracesdk/testframework/export/ExportedTelemetryParityValidator.kt
+++ embrace-android-sdk/src/integrationTest/kotlin/io/embrace/android/embracesdk/testframework/export/ExportedTelemetryParityValidator.kt
@@ -8,6 +8,7 @@ import io.opentelemetry.kotlin.aliases.OtelJavaLogRecordData
 import io.opentelemetry.kotlin.aliases.OtelJavaSpanData
 import io.opentelemetry.kotlin.semconv.ExceptionAttributes
 import io.opentelemetry.kotlin.semconv.SessionAttributes
+import io.opentelemetry.kotlin.semconv.TelemetryAttributes
 import org.junit.Assert.assertEquals
 
 /**
@@ -142,6 +143,7 @@ internal class ExportedTelemetryParityValidator {
          * stringifies as its wrapper - 'LongValue(value=3)' rather than '3'.
          */
         private val REDACTED_ATTRIBUTES = setOf(
+            TelemetryAttributes.TELEMETRY_DISTRO_VERSION,
             SessionAttributes.SESSION_ID,
             EmbSessionAttributes.EMB_USER_SESSION_ID,
             EmbSessionAttributes.EMB_SESSION_PART_ID,

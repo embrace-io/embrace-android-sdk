307d11271f4cd11f687c1c490a9481eb92153459
Jamie Lynch <fractalwrench@gmail.com>
2026-08-11 09:21:32 +0100

    perf: avoid storing screen resolution in shared prefs (#3763)

--- embrace-android-core/src/main/kotlin/io/embrace/android/embracesdk/internal/injection/PayloadSourceModuleImpl.kt
+++ embrace-android-core/src/main/kotlin/io/embrace/android/embracesdk/internal/injection/PayloadSourceModuleImpl.kt
@@ -86,10 +86,9 @@ class PayloadSourceModuleImpl(
             device = EmbTrace.trace("deviceImpl") {
                 DeviceImpl(
                     windowManagerProvider = { coreModule.context.getSystemServiceSafe(Context.WINDOW_SERVICE) },
-                    coreModule.store,
-                    workerThreadModule.backgroundWorker(Worker.Background.NonIoRegWorker),
-                    initModule.systemInfo,
-                    initModule.logger,
+                    backgroundWorker = workerThreadModule.backgroundWorker(Worker.Background.NonIoRegWorker),
+                    systemInfo = initModule.systemInfo,
+                    logger = initModule.logger,
                 )
             },
             rnBundleIdProvider = { rnBundleIdTracker.getReactNativeBundleId() },

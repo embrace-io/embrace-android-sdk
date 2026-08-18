be2df57be0cdda0ca1b3516cfb644fa005bc59b0
Jamie Lynch <fractalwrench@gmail.com>
2026-08-18 17:27:55 +0100

    implement limit on file size when reconstructing session

---
+++ embrace-android-session-persistence/src/main/kotlin/io/embrace/android/embracesdk/internal/session/persistence/SessionPartLimits.kt
@@ -0,0 +1,7 @@
+package io.embrace.android.embracesdk.internal.session.persistence
+
+/**
+ * Upper bound on the size of a session part file. This is meant as an upper bound to prevent memory
+ * exhaustion and is considered unlikely for the vast majority of runs.
+ */
+internal const val MAX_PART_FILE_BYTES: Long = 3L * 1024 * 1024

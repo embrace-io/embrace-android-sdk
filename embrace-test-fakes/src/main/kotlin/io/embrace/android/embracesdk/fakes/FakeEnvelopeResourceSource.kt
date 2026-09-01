04cc90c8ec9801f3875ba925d629c8d992cafb19
Jamie Lynch <fractalwrench@gmail.com>
2026-09-01 10:40:51 +0100

    provide callback when envelope resource mutates (#3883)

--- embrace-test-fakes/src/main/kotlin/io/embrace/android/embracesdk/fakes/FakeEnvelopeResourceSource.kt
+++ embrace-test-fakes/src/main/kotlin/io/embrace/android/embracesdk/fakes/FakeEnvelopeResourceSource.kt
@@ -7,10 +7,21 @@ class FakeEnvelopeResourceSource : EnvelopeResourceSource {
 
     var resource: EnvelopeResource = EnvelopeResource()
     var customValues = mutableMapOf<String, String>()
+    val listeners = mutableListOf<(EnvelopeResource) -> Unit>()
 
     override fun getEnvelopeResource(): EnvelopeResource = resource
 
     override fun add(key: String, value: String) {
         customValues[key] = value
+    }
+
+    override fun addChangeListener(listener: (EnvelopeResource) -> Unit) {
+        listeners.add(listener)
+        listener(resource)
+    }
+
+    fun changeResource(resource: EnvelopeResource) {
+        this.resource = resource
+        listeners.forEach { it(resource) }
     }
 }

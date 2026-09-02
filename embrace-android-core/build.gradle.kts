20876f872a4b8d34563aa1884be813c0476ae655
Jamie Lynch <fractalwrench@gmail.com>
2026-09-02 11:22:07 +0100

    build: add module for telemetry limits (#3923)

--- embrace-android-core/build.gradle.kts
+++ embrace-android-core/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation(project(":embrace-android-delivery"))
     implementation(project(":embrace-android-envelope"))
     implementation(project(":embrace-android-infra"))
+    implementation(project(":embrace-android-limits"))
     implementation(project(":embrace-android-utils"))
     implementation(project(":embrace-internal-api"))
     implementation(project(":embrace-android-otel"))

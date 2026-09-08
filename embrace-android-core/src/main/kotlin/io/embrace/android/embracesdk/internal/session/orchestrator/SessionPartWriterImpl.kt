bc63f5d1e585b7eb2d39c998d168e9555e283413
Jamie Lynch <fractalwrench@gmail.com>
2026-09-08 10:23:59 +0100

    increase debounce time (#3972)

--- embrace-android-core/src/main/kotlin/io/embrace/android/embracesdk/internal/session/orchestrator/SessionPartWriterImpl.kt
+++ embrace-android-core/src/main/kotlin/io/embrace/android/embracesdk/internal/session/orchestrator/SessionPartWriterImpl.kt
@@ -58,9 +58,9 @@ class SessionPartWriterImpl(
         private const val MAX_CARRIED_OVER_SPANS: Int = 1000
         private const val CARRIED_OVER_SPAN_LIMIT_TYPE: String = "carried_over_span"
 
-        const val METADATA_WRITE_DELAY_MS: Long = 10
-        const val SESSION_SPAN_WRITE_DELAY_MS: Long = 100
-        const val SPAN_SNAPSHOT_WRITE_DELAY_MS: Long = 100
+        const val METADATA_WRITE_DELAY_MS: Long = 500
+        const val SESSION_SPAN_WRITE_DELAY_MS: Long = 1000
+        const val SPAN_SNAPSHOT_WRITE_DELAY_MS: Long = 1000
     }
 
     /**
